1d1781e2cd6954ba7dd0c935da994c9ea8934f6d
nicksieger <nicksieger@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2007-03-24 04:08:55 +0000

    Removing obsolete files
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/activerecord-jdbc@386 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- init.rb
+++
@@ -1,8 +0,0 @@
-RAILS_CONNECTION_ADAPTERS << 'jdbc'
-require 'active_record/connection_adapters/jdbc_adapter'
-
-[:initialize_database, :initialize_framework_logging, :initialize_framework_settings].each do |cmd|
-  Rails::Initializer.run(cmd) do |config|
-    config.frameworks = [:active_record]
-  end
-end
