d2a30dd9faa4911d264e4a8903f5897fc9e4365d
olabini <olabini@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2007-07-20 13:15:48 +0000

    Make Oracle driver actually work
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/activerecord-jdbc@671 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- lib/jdbc_adapter/jdbc_oracle.rb
+++ lib/jdbc_adapter/jdbc_oracle.rb
@@ -2,7 +2,7 @@ module ::JdbcSpec
   module ActiveRecordExtensions
     def oracle_connection(config)
       config[:port] ||= 1521
-      config[:url] ||= "jdbc:oracle:thin://#{config[:host]}:#{config[:port]}:#{config[:database]}"
+      config[:url] ||= "jdbc:oracle:thin:@#{config[:host]}:#{config[:port]}:#{config[:database]}"
       config[:driver] ||= "oracle.jdbc.driver.OracleDriver"
       jdbc_connection(config)
     end
