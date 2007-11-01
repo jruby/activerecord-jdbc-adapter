2e76e7a4dcc523f541c78b3d3bcd0abd114285d5
olabini <olabini@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2007-11-01 18:53:24 +0000

    Make sure to warn if the driver class isn't present. The error you get at this point otherwise is not really enlightening.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/activerecord-jdbc@781 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- lib/active_record/connection_adapters/jdbc_adapter.rb
+++ lib/active_record/connection_adapters/jdbc_adapter.rb
@@ -203,17 +203,19 @@ module ActiveRecord
 
       def driver_class
         @driver_class ||= begin
-          driver_class_const = (@name[0...1].capitalize + @name[1..@name.length]).gsub(/\./, '_')
-          Jdbc::Mutex.synchronized do
-            unless Jdbc.const_defined?(driver_class_const)
-              driver_class_name = @name
-              Jdbc.module_eval do
-                include_class(driver_class_name) { driver_class_const }
-              end
-            end
-          end
-          Jdbc.const_get(driver_class_const)
-        end
+                            driver_class_const = (@name[0...1].capitalize + @name[1..@name.length]).gsub(/\./, '_')
+                            Jdbc::Mutex.synchronized do
+                              unless Jdbc.const_defined?(driver_class_const)
+                                driver_class_name = @name
+                                Jdbc.module_eval do
+                                  include_class(driver_class_name) { driver_class_const }
+                                end
+                              end
+                            end
+                            driver_class = Jdbc.const_get(driver_class_const)
+                            raise "You specify a driver for your JDBC connection" unless driver_class
+                            driver_class
+                          end
       end
 
       def load
