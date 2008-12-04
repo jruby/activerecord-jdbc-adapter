5692af58b673fd42a92896cf9e473b65cce0c52b
Nils Christian Haugen <nchaugen@gmail.com>
2008-12-05 00:10:42 +0100

    Added has_many_through test to sqlite3 test

--- test/sqlite3_simple_test.rb
+++ test/sqlite3_simple_test.rb
@@ -4,3 +4,7 @@ require 'db/sqlite3'
 class SQLite3SimpleTest < Test::Unit::TestCase
   include SimpleTestMethods
 end
+
+class SQLite3HasManyThroughTest < Test::Unit::TestCase
+  include HasManyThroughMethods
+end
