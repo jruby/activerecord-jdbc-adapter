f71fcb67fa3b4e4ee75b318d4b12781ebaa9adcf
David Kellum <dek-oss@gravitext.com>
2009-05-07 00:53:26 -0700

    Upgraded to 8.3-604 drivers;  Added runtime selection of either JDBC 3 or JDBC 4 (j2se 1.6+)  driver.

--- drivers/postgres/lib/jdbc/postgres.rb
+++ drivers/postgres/lib/jdbc/postgres.rb
@@ -1,10 +1,25 @@
 module Jdbc
   module Postgres
-    VERSION = "8.2" 
+    VERSION = "8.3.604"
+
+    def self.require_driver_jar
+      vers  = VERSION.split( '.' )
+      vers << jdbc_version
+      require( "postgresql-%s.%s-%s.jdbc%d.jar" % vers )
+    end
+
+    # JDBC version 4 if Java >=1.6, else 3
+    def self.jdbc_version
+      vers = Java::java.lang.System::get_property( "java.specification.version" )
+      vers = vers.split( '.' ).map { |v| v.to_i }
+      ( ( vers <=> [ 1, 6 ] ) >= 0 ) ? 4 : 3
+    end
+
   end
 end
+
 if RUBY_PLATFORM =~ /java/
-  require "postgresql-#{Jdbc::Postgres::VERSION}-504.jdbc3.jar"
+  Jdbc::Postgres::require_driver_jar
 else
   warn "jdbc-postgres is only for use with JRuby"
 end
