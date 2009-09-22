ab08e0f01b102d294a41251bc135561467c1d526
Ryan Baumann <ryan.baumann@gmail.com>
2009-09-22 15:20:29 -0400

    Import a bunch of useful methods from AR's sqlite_adapter, then switch rename_column to duplicate the behavior of the AR method

--- lib/jdbc_adapter/jdbc_sqlite3.rb
+++ lib/jdbc_adapter/jdbc_sqlite3.rb
@@ -232,7 +232,10 @@ module ::JdbcSpec
     end
 
     def rename_column(table_name, column_name, new_column_name) #:nodoc:
-      execute "ALTER TABLE #{table_name} ALTER COLUMN #{column_name} RENAME TO #{new_column_name}"
+      unless columns(table_name).detect{|c| c.name == column_name.to_s }
+        raise ActiveRecord::ActiveRecordError, "Missing column #{table_name}.#{column_name}"
+      end
+      alter_table(table_name, :rename => {column_name.to_s => new_column_name.to_s})
     end
 
     def rename_table(name, new_name)
@@ -278,6 +281,11 @@ module ::JdbcSpec
         ::ActiveRecord::ConnectionAdapters::IndexDefinition.new(table_name, name, unique, cols)
       end
     end
+    
+    def primary_key(table_name) #:nodoc:
+      column = table_structure(table_name).find {|field| field['pk'].to_i == 1}
+      column ? column['name'] : nil
+    end
 
     def recreate_database(name)
       tables.each{ |table| drop_table(table) }
@@ -320,6 +328,86 @@ module ::JdbcSpec
     def add_lock!(sql, options) #:nodoc:
       sql
     end
+    
+    protected
+      def alter_table(table_name, options = {}) #:nodoc:
+        altered_table_name = "altered_#{table_name}"
+        caller = lambda {|definition| yield definition if block_given?}
+
+        transaction do
+          move_table(table_name, altered_table_name,
+            options.merge(:temporary => true))
+          move_table(altered_table_name, table_name, &caller)
+        end
+      end
+      
+      def move_table(from, to, options = {}, &block) #:nodoc:
+        copy_table(from, to, options, &block)
+        drop_table(from)
+      end
+      
+      def copy_table(from, to, options = {}) #:nodoc:
+        options = options.merge(:id => (!columns(from).detect{|c| c.name == 'id'}.nil? && 'id' == primary_key(from).to_s))
+        create_table(to, options) do |definition|
+          @definition = definition
+          columns(from).each do |column|
+            column_name = options[:rename] ?
+              (options[:rename][column.name] ||
+               options[:rename][column.name.to_sym] ||
+               column.name) : column.name
+            
+            @definition.column(column_name, column.type,
+              :limit => column.limit, :default => column.default,
+              :null => column.null)
+          end
+          @definition.primary_key(primary_key(from)) if primary_key(from)
+          yield @definition if block_given?
+        end
+
+        copy_table_indexes(from, to, options[:rename] || {})
+        copy_table_contents(from, to,
+          @definition.columns.map {|column| column.name},
+          options[:rename] || {})
+      end
+      
+      def copy_table_indexes(from, to, rename = {}) #:nodoc:
+        indexes(from).each do |index|
+          name = index.name
+          if to == "altered_#{from}"
+            name = "temp_#{name}"
+          elsif from == "altered_#{to}"
+            name = name[5..-1]
+          end
+
+          to_column_names = columns(to).map(&:name)
+          columns = index.columns.map {|c| rename[c] || c }.select do |column|
+            to_column_names.include?(column)
+          end
+
+          unless columns.empty?
+            # index name can't be the same
+            opts = { :name => name.gsub(/_(#{from})_/, "_#{to}_") }
+            opts[:unique] = true if index.unique
+            add_index(to, columns, opts)
+          end
+        end
+      end
+
+      def copy_table_contents(from, to, columns, rename = {}) #:nodoc:
+        column_mappings = Hash[*columns.map {|name| [name, name]}.flatten]
+        rename.inject(column_mappings) {|map, a| map[a.last] = a.first; map}
+        from_columns = columns(from).collect {|col| col.name}
+        columns = columns.find_all{|col| from_columns.include?(column_mappings[col])}
+        quoted_columns = columns.map { |col| quote_column_name(col) } * ','
+
+        quoted_to = quote_table_name(to)
+        execute "SELECT * FROM #{quote_table_name(from)}" do |row|
+          sql = "INSERT INTO #{quoted_to} (#{quoted_columns}) VALUES ("
+          sql << columns.map {|col| quote row[column_mappings[col]]} * ', '
+          sql << ')'
+          execute sql
+        end
+      end
   end
 end
 
