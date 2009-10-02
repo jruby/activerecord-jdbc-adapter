b71e925ae9d6bfecbc4a5c23eb303fa2f6cc9a32
Nick Sieger <nick@nicksieger.com>
2009-10-02 12:05:19 -0500

    Add stub 'pg' library for postgresql; to allow Rails to be able to load

---
+++ lib/pg.rb
@@ -0,0 +1,4 @@
+# Stub library for postgresql -- allows Rails to load
+# postgresql_adapter without error Other than postgres-pr, there's no
+# other way to use PostgreSQL on JRuby anyway, right? If you've
+# installed ar-jdbc you probably want to use that to connect to pg.
