fbd0c1e021fed65031ac81c3c6bf47032d268386
Mike Williams <mdub@dogbiscuit.org>
2010-03-16 16:04:11 +1100

    Suppress #test_save_date for MS-SQL, which doesn't have a DATE class.

--- test/mssql_simple_test.rb
+++ test/mssql_simple_test.rb
@@ -2,5 +2,10 @@ require 'jdbc_common'
 require 'db/mssql'
 
 class MsSQLSimpleTest < Test::Unit::TestCase
+
   include SimpleTestMethods
+  
+  # Suppress the date round-tripping test.  MS SQL 2005 doesn't have a DATE class, only TIMESTAMP.
+  undef_method :test_save_date
+  
 end
