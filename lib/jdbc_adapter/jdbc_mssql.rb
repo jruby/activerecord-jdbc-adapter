2602c036511949d3e7cba3173f3589ec1a7d271d
Mike Williams <mdub@dogbiscuit.org>
2010-03-17 11:33:32 +1100

    String-unquoting only needs to happen for default values (supercedes the fix in JRUBY-2011).

--- lib/jdbc_adapter/jdbc_mssql.rb
+++ lib/jdbc_adapter/jdbc_mssql.rb
@@ -71,10 +71,14 @@ module ::JdbcSpec
         end
       end
 
+      def default_value(value)
+        return $1 if value =~ /^\(N?'(.*)'\)$/
+        value
+      end
+      
       def type_cast(value)
         return nil if value.nil? || value == "(null)" || value == "(NULL)"
         case type
-        when :string then unquote_string value
         when :integer then unquote(value).to_i rescue value ? 1 : 0
         when :primary_key then value == true || value == false ? value == true ? 1 : 0 : value.to_i
         when :decimal   then self.class.value_to_decimal(unquote(value))
@@ -91,11 +95,6 @@ module ::JdbcSpec
 
       def is_utf8?
         sql_type =~ /nvarchar|ntext|nchar/i
-      end
-
-      # JRUBY-2011: Match balanced quotes and parenthesis - 'text',('text') or (text)
-      def unquote_string(value)
-        value.to_s.sub(/^\((.*)\)$/,'\1').sub(/^N?'(.*)'$/,'\1')
       end
 
       def unquote(value)
--- test/mssql_simple_test.rb
+++ test/mssql_simple_test.rb
@@ -4,8 +4,17 @@ require 'db/mssql'
 class MsSQLSimpleTest < Test::Unit::TestCase
 
   include SimpleTestMethods
-  
+
   # Suppress the date round-tripping test.  MS SQL 2005 doesn't have a DATE class, only TIMESTAMP.
   undef_method :test_save_date
-  
+
+  def test_does_not_munge_quoted_strings
+    example_quoted_values = [%{'quoted'}, %{D'oh!}]
+    example_quoted_values.each do |value|
+      entry = Entry.create!(:title => value)
+      entry.reload
+      assert_equal(value, entry.title)
+    end
+  end
+
 end
