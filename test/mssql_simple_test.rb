d97d3aaf817f81fbfbc78829951e024b1bc2b691
Mike Williams <mdub@dogbiscuit.org>
2010-03-17 20:58:06 +1100

    Be explicit about disabling standard tests for MS-SQL (and explain why).

--- test/mssql_simple_test.rb
+++ test/mssql_simple_test.rb
@@ -5,8 +5,11 @@ class MsSQLSimpleTest < Test::Unit::TestCase
 
   include SimpleTestMethods
 
-  # Suppress the date round-tripping test.  MS SQL 2005 doesn't have a DATE class, only TIMESTAMP.
+  # MS SQL 2005 doesn't have a DATE class, only TIMESTAMP.
   undef_method :test_save_date
+
+  # String comparisons are insensitive by default
+  undef_method :test_validates_uniqueness_of_strings_case_sensitive
 
   def test_does_not_munge_quoted_strings
     example_quoted_values = [%{'quoted'}, %{D'oh!}]
