220f2af632d30038a1016c8f3ab89579642195e0
Nick Sieger <nick@nicksieger.com>
2010-04-22 14:33:07 -0500

    Allow override of host where sqlserver is running through ENV['SQLHOST']

--- test/db/mssql.rb
+++ test/db/mssql.rb
@@ -1,9 +1,9 @@
-host = ENV['SQLHOST'] || 'localhost'
 config = {
   :username => 'blog',
   :password => '',
   :adapter  => 'mssql',
   :database => 'weblog_development'
 }
+config[:host] = ENV['SQLHOST'] if ENV['SQLHOST']
 
 ActiveRecord::Base.establish_connection( config )
