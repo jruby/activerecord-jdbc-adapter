9ecf8172ae0ab7ed0d707a33d29cd4f2cbe5a3c1
Nick Sieger <nick@nicksieger.com>
2010-05-04 20:20:08 -0500

    ACTIVERECORD_JDBC-100: Add test verifying that synonym columns are the same as the original table

--- test/oracle_simple_test.rb
+++ test/oracle_simple_test.rb
@@ -12,6 +12,7 @@ class OracleSpecificTest < Test::Unit::TestCase
     super
     @java_con.createStatement.execute "CREATE TABLE DEFAULT_NUMBER (VALUE NUMBER, DATUM DATE)"
     @java_con.createStatement.execute "INSERT INTO DEFAULT_NUMBER (VALUE, DATUM) VALUES (0.076, TIMESTAMP'2009-11-05 00:00:00')"
+    @java_con.createStatement.execute "CREATE SYNONYM POSTS FOR ENTRIES"
     @klass = Class.new(ActiveRecord::Base)
     @klass.set_table_name "DEFAULT_NUMBER"
   end
@@ -37,4 +38,16 @@ class OracleSpecificTest < Test::Unit::TestCase
     obj = @klass.find(:first)
     assert obj.datum.nil?
   end
+
+  def test_model_access_by_synonym
+    @klass.set_table_name "POSTS"
+    entry_columns = Entry.columns_hash
+    @klass.columns.each do |c|
+      ec = entry_columns[c.name]
+      assert ec
+      assert_equal ec.sql_type, c.sql_type
+      assert_equal ec.type, c.type
+    end
+  end
+
 end if defined?(JRUBY_VERSION)
