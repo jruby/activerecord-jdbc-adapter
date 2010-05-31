jruby/activerecord-jdbc-adapter

commit aa760d412fd6a530769e617568643a00a571fce3

ACTIVERECORD_JDBC-116: Fix handling of "schema_name.table_name" in MySQL (Dilshod Mukhtarov)

src/java/jdbc_adapter/RubyJdbcConnection.java
@@ -135,15 +135,18 @@ public class RubyJdbcConnection extends RubyObject {
                     if (schemaName != null) schemaName = caseConvertIdentifierForJdbc(metadata, schemaName);
                     table_name = caseConvertIdentifierForJdbc(metadata, table_name);
 
+                    String catalog = c.getCatalog();
+                    if (schemaName != null) { catalog = schemaName; }
+
                     String[] tableTypes = new String[]{"TABLE","VIEW","SYNONYM"};
                     RubyArray matchingTables = (RubyArray) tableLookupBlock(context.getRuntime(),
-                            c.getCatalog(), schemaName, table_name, tableTypes, false).call(c);
+                            catalog, schemaName, table_name, tableTypes, false).call(c);
                     if (matchingTables.isEmpty()) {
                         throw new SQLException("Table " + table_name + " does not exist");
                     }
 
-                    results = metadata.getColumns(c.getCatalog(),schemaName,table_name,null);
-                    pkeys = metadata.getPrimaryKeys(c.getCatalog(),schemaName,table_name);
+                    results = metadata.getColumns(catalog,schemaName,table_name,null);
+                    pkeys = metadata.getPrimaryKeys(catalog,schemaName,table_name);
                     return unmarshal_columns(context, metadata, results, pkeys);
                 } finally {
                     close(results);

commit 72e5dc2ba8c2fe2a91352ef35e48664cbe5214f7

Fix problems introduced by commit aa760d412fd6a530769e - Overriding the catalog with the schema will break a MS SQL server database which uses schemas

src/java/jdbc_adapter/MssqlRubyJdbcConnection.java
@@ -79,4 +79,13 @@ public class MssqlRubyJdbcConnection extends RubyJdbcConnection {
         }
         return super.jdbcToRuby(runtime, column, type, resultSet);
     }
+    
+    /**
+     * SqlServer supports schemas so lets not go using the schema name as the catalog name
+     */
+    @Override
+    protected boolean databaseSupportsSchemas()
+    {
+        return true;
+    }
 }

src/java/jdbc_adapter/RubyJdbcConnection.java
@@ -136,7 +136,8 @@ public class RubyJdbcConnection extends RubyObject {
                     table_name = caseConvertIdentifierForJdbc(metadata, table_name);
 
                     String catalog = c.getCatalog();
-                    if (schemaName != null) { catalog = schemaName; }
+                    
+                    if (schemaName != null && !databaseSupportsSchemas()) { catalog = schemaName; }
 
                     String[] tableTypes = new String[]{"TABLE","VIEW","SYNONYM"};
                     RubyArray matchingTables = (RubyArray) tableLookupBlock(context.getRuntime(),
@@ -1172,5 +1173,10 @@ public class RubyJdbcConnection extends RubyObject {
 
             return columns;
         }
+    }
+    
+    protected boolean databaseSupportsSchemas()
+    {
+      return false;
     }
 }