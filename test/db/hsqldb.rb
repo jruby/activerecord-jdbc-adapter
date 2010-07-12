e30b9748a31f736cda806fc7354dd69924ba0b9c
Nick Sieger <nick@nicksieger.com>
2010-07-12 13:37:05 -0500

    Small tweak to the way hsqldb is cleaned up to be compatible with 2.0

--- test/db/hsqldb.rb
+++ test/db/hsqldb.rb
@@ -7,6 +7,7 @@ ActiveRecord::Base.establish_connection(config)
 
 at_exit {
   # Clean up hsqldb when done
-  Dir['test.db*'].each {|f| File.delete(f)}
-  File.delete('hsqldb-testdb.log') rescue nil #can't delete on windows
+  require "fileutils"
+  Dir['test.db*'].each {|f| FileUtils.rm_rf(f)}
+  FileUtils.rm_rf('hsqldb-testdb.log') rescue nil #can't delete on windows
 }
