463762f8166a71729ced8a08cea3e1eb8ab2b7b5
Nick Sieger <nick@nicksieger.com>
2010-07-15 13:21:15 -0500

    Handle MySQL integer type sizes better

--- lib/arjdbc/mysql/adapter.rb
+++ lib/arjdbc/mysql/adapter.rb
@@ -38,6 +38,7 @@ module ::ArJdbc
 
     def modify_types(tp)
       tp[:primary_key] = "int(11) DEFAULT NULL auto_increment PRIMARY KEY"
+      tp[:integer] = { :name => 'int', :limit => 4 }
       tp[:decimal] = { :name => "decimal" }
       tp[:timestamp] = { :name => "datetime" }
       tp[:datetime][:limit] = nil
@@ -221,6 +222,19 @@ module ::ArJdbc
 
     def collation
       show_variable("collation_database")
+    end
+
+    def type_to_sql(type, limit = nil, precision = nil, scale = nil)
+      return super unless type.to_s == 'integer'
+
+      case limit
+      when 1; 'tinyint'
+      when 2; 'smallint'
+      when 3; 'mediumint'
+      when nil, 4, 11; 'int(11)'  # compatibility with MySQL default
+      when 5..8; 'bigint'
+      else raise(ActiveRecordError, "No integer type has byte size #{limit}")
+      end
     end
 
     protected
