c4a0dac4bfd9f927bbe17cc0be1aa1bdeb620ee5
Dwayne Litzenberger <dlitz@infonium.ca>
2010-08-09 15:02:01 -0400

    mssql: Fix 'rake db:schema:load' with boolean, date, datetime columns

--- lib/arjdbc/mssql/adapter.rb
+++ lib/arjdbc/mssql/adapter.rb
@@ -67,6 +67,8 @@ module ::ArJdbc
       # See: http://msdn.microsoft.com/en-us/library/ms186939.aspx
       if type.to_s == 'string' and limit == 1073741823 and sqlserver_version != "2000"
         'NVARCHAR(MAX)'
+      elsif %w( boolean date datetime ).include?(type.to_s)
+        super(type)   # cannot specify limit/precision/scale with these types
       else
         super
       end
