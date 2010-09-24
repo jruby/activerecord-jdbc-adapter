e6a93378a29a2e557cb34e2442b669671cda20ef
Nick Sieger <nick@nicksieger.com>
2010-09-23 19:28:55 -0500

    JRUBY-1642: Don't use H2 INFORMATION_SCHEMA in table or column searches

--- lib/arjdbc/h2/adapter.rb
+++ lib/arjdbc/h2/adapter.rb
@@ -11,5 +11,18 @@ module ArJdbc
     def h2_adapter
       true
     end
+
+    def tables
+      @connection.tables(nil, h2_schema)
+    end
+
+    def columns(table_name, name=nil)
+      @connection.columns_internal(table_name.to_s, name, h2_schema)
+    end
+
+    private
+    def h2_schema
+      @config[:schema] || ''
+    end
   end
 end
