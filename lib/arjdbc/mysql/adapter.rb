489eb3c45978bf842c3f95e6061946187fd3911b
Toms Mikoss <t.mikoss@alise.lv>
2010-10-29 15:57:37 +0300

    Added 'date' column type to case statement that extracts column limits, to prevent generation of invalid schema.rb files

--- lib/arjdbc/mysql/adapter.rb
+++ lib/arjdbc/mysql/adapter.rb
@@ -58,7 +58,7 @@ module ::ArJdbc
         when /^mediumint/i; 3
         when /^smallint/i;  2
         when /^tinyint/i;   1
-        when /^(datetime|timestamp)/
+        when /^(datetime|timestamp|date)/
           nil
         else
           super
