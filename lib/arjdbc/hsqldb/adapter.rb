9a117114a8dae904b29e896ead1d60ac9f00a3c8
Denis Odorcic <denis.odorcic@gmail.com>
2010-11-19 01:03:58 -0500

    Remove some useless checks and sql calls in add_column for setting NULL

--- lib/arjdbc/hsqldb/adapter.rb
+++ lib/arjdbc/hsqldb/adapter.rb
@@ -104,16 +104,9 @@ module ::ArJdbc
     end
 
     def add_column(table_name, column_name, type, options = {})
-      if option_not_null = (options[:null] == false)
-        options.delete(:null)
-      end
       add_column_sql = "ALTER TABLE #{quote_table_name(table_name)} ADD #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
       add_column_options!(add_column_sql, options)
       execute(add_column_sql)
-      if option_not_null
-        alter_column_sql = "ALTER TABLE #{quote_table_name(table_name)} ALTER COLUMN #{quote_column_name(column_name)} SET NOT NULL"
-        execute(alter_column_sql)
-      end
     end
 
     def change_column(table_name, column_name, type, options = {}) #:nodoc:
