68d0bc4a653bec98a1e532c9c7509aa3675fa2a3
Nick Sieger <nick@nicksieger.com>
2010-12-29 12:41:01 -0600

    JRUBY-5188: Be less aggressive when trying to load Railties

--- lib/activerecord-jdbc-adapter.rb
+++ lib/activerecord-jdbc-adapter.rb
@@ -1,6 +1,8 @@
 require 'arjdbc'
-begin
-  require 'arjdbc/jdbc/railtie'
-rescue LoadError
-  # Assume we don't have railties in this version of AR
+if ActiveRecord::VERSION::MAJOR >= 3
+  begin
+    require 'arjdbc/jdbc/railtie'
+  rescue LoadError
+    # Assume we don't have railties in this version of AR
+  end
 end
