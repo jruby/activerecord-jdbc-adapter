fe76d425b784da4ef6048df53348ece80d3f2170
Nick Sieger <nick@nicksieger.com>
2011-06-14 13:35:48 -0500

    Update to sqlite-jdbc 3.7.2 from http://www.xerial.org/trac/Xerial/wiki/SQLiteJDBC
    fixes #21.

--- drivers/sqlite3/lib/jdbc/sqlite3.rb
+++ drivers/sqlite3/lib/jdbc/sqlite3.rb
@@ -1,10 +1,10 @@
 module Jdbc
   module SQLite3
-    VERSION = "3.6.14.2.056" # Based on SQLite 3.6.14.2
+    VERSION = "3.7.2"
   end
 end
 if RUBY_PLATFORM =~ /java/
-  require "sqlitejdbc-#{Jdbc::SQLite3::VERSION}.jar"
+  require "sqlite-jdbc-#{Jdbc::SQLite3::VERSION}.jar"
 else
   warn "jdbc-SQLite3 is only for use with JRuby"
 end
