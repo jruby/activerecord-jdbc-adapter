066f14c6c188d6b39a005d09caf391d2b8fbc558
Nick Sieger <nick@nicksieger.com>
2011-06-17 13:18:14 -0500

    Make a stub Mysql::Error class, for AR's databases.rake file.
    Also throw a real exception for general connection failure. Fixes #19.

--- lib/arjdbc/mysql/adapter.rb
+++ lib/arjdbc/mysql/adapter.rb
@@ -414,6 +414,11 @@ module ActiveRecord::ConnectionAdapters
 end
 
 module Mysql                    # :nodoc:
+  remove_const(:Error) if const_defined?(:Error)
+
+  class Error < ::ActiveRecord::JDBCError
+  end
+
   def self.client_version
     50400                       # faked out for AR tests
   end
