107dac663de6b491780a95c19226ad8e64cf5f3f
Marcus Brito <marcus.brito@ecore.com.br>
2011-08-05 10:32:07 -0300

    Support for key lengths in #add_index for the MySQL adapter.

--- test/mysql_index_length_test.rb
+++ test/mysql_index_length_test.rb
@@ -6,6 +6,7 @@ class MySQLIndexLengthDBSetup < ActiveRecord::Migration
     execute <<-SQL
       CREATE TABLE index_length_test (
         text_column TEXT,
+        second_text_column TEXT,
         INDEX ix_length_text (text_column(255))
       )
     SQL
@@ -36,5 +37,15 @@ class MySQLIndexLengthTest < Test::Unit::TestCase
     assert !index.unique
     assert_equal ["text_column"], index.columns
     assert_equal [255], index.lengths
+  end
+
+  def test_add_index
+    @connection.add_index 'index_length_test', ['text_column', 'second_text_column'],
+      :name => 'added_index', :length => {'text_column' => 32, 'second_text_column' => 64}
+
+    index = @connection.indexes('index_length_test').find { |idx| idx.name == 'added_index' }
+    assert_not_nil index
+    assert_equal ['text_column', 'second_text_column'], index.columns
+    assert_equal [32, 64], index.lengths
   end
 end
