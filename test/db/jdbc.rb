f10885c88b342afab10769d789b8a9f821ffd5e2
Nick Sieger <nick@nicksieger.com>
2011-11-29 11:53:09 -0600

    Change db credentials for generic connection test

--- test/db/jdbc.rb
+++ test/db/jdbc.rb
@@ -1,11 +1,12 @@
 require 'jdbc/mysql'
 
 config = {
-  :username => 'blog',
-  :password => '',
+  # see db/mysql.rb
+  :username => 'arjdbc',
+  :password => 'arjdbc',
   :adapter  => 'jdbc',
   :driver   => 'com.mysql.jdbc.Driver',
-  :url      => 'jdbc:mysql://localhost:3306/weblog_development'
+  :url      => 'jdbc:mysql://localhost:3306/arjdbc_test'
 }
 
 ActiveRecord::Base.establish_connection(config)
