c6f0605da36af1f5f4ba1faa2da39d8541f67722
Dwayne Litzenberger <dlitz@patientway.com>
2012-01-11 10:40:24 -0500

    derby: TIMESTAMP means :datetime in Derby

--- lib/arjdbc/derby/adapter.rb
+++ lib/arjdbc/derby/adapter.rb
@@ -40,6 +40,7 @@ module ::ArJdbc
         case field_type
         when /smallint/i  then :boolean
         when /real/i      then :float
+        when /^timestamp/i then :datetime
         else
           super
         end
@@ -73,10 +74,10 @@ module ::ArJdbc
     # In Derby, the following cannot specify a limit:
     # - integer
     # - boolean (smallint)
-    # - timestamp
+    # - datetime (timestamp)
     # - date
     def type_to_sql(type, limit = nil, precision = nil, scale = nil) #:nodoc:
-      return super unless [:integer, :boolean, :timestamp, :date].include? type
+      return super unless [:integer, :boolean, :timestamp, :datetime, :date].include? type
 
       native = native_database_types[type.to_s.downcase.to_sym]
       native.is_a?(Hash) ? native[:name] : native
@@ -87,6 +88,7 @@ module ::ArJdbc
       tp[:string][:limit] = 256
       tp[:integer][:limit] = nil
       tp[:boolean] = {:name => "smallint"}
+      tp[:datetime] = {:name => "timestamp", :limit => nil}
       tp[:timestamp][:limit] = nil
       tp[:date][:limit] = nil
       tp
--- test/derby_simple_test.rb
+++ test/derby_simple_test.rb
@@ -96,4 +96,44 @@ class DerbySimpleTest < Test::Unit::TestCase
       end
     end
   end
+
+  def test_data_types
+    # From test/models/data_types.rb, with the modifications as noted in the comments.
+    expected_types = [
+      ["id",                          :integer,   { }],
+      ["sample_timestamp",            :datetime,  { }],   # :timestamp is just an alias for :datetime in Derby
+      ["sample_datetime",             :datetime,  { }],
+      ["sample_date",                 :date,      { }],
+      ["sample_time",                 :time,      { }],
+      ["sample_decimal",              :integer,   { :precision => 15, :scale => 0 }],   # it's an :integer because the :scale is 0 (...right?)
+      ["sample_small_decimal",        :decimal,   { :precision => 3,  :scale => 2 }],
+      ["sample_default_decimal",      :integer,   { }],                 # decimal and integer are the same type in Derby
+      ["sample_float",                :float,     { }],
+      ["sample_binary",               :binary,    { }],
+      ["sample_boolean",              :boolean,   { }],
+      ["sample_string",               :string,    { :default => '' }],
+      ["sample_integer",              :integer,   { }],                 # don't care about the limit
+      ["sample_integer_with_limit_2", :integer,   { }],                 # don't care about the limit
+      ["sample_integer_with_limit_8", :integer,   { }],                 # don't care about the limit
+      ["sample_integer_no_limit",     :integer,   { }],
+      ["sample_integer_neg_default",  :integer,   { :default => -1 }],
+      ["sample_text",                 :text,      { }],
+    ].sort{|a,b| a[0] <=> b[0]}
+
+    column_names = (expected_types.map{|et| et[0]} + DbType.column_names).sort.uniq
+    result = []
+    column_names.each do |column_name|
+      et = expected_types.detect{|t| t[0] == column_name }
+      col = DbType.columns_hash[column_name]
+      if col
+        attrs = et && Hash[et[2].keys.map{|k| [k, col.send(k)]}]
+        result << [col.name, col.type, attrs]
+      else
+        result << [column_name, nil, nil]
+      end
+    end
+    result.sort!{|a,b| a[0] <=> b[0]}
+
+    assert_equal expected_types, result
+  end
 end
