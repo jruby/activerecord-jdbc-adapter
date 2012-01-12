e114a4b499785d97db9e6f716fc0fcf11be51f2d
Dwayne Litzenberger <dlitz@patientway.com>
2012-01-12 16:04:07 -0500

    derby: Fix order of limit & offset clauses
    In Derby, the limit clause is "FETCH FIRST n ROWS ONLY" and the offset clause
    is "OFFSET m ROWS", and the offset clause must come *before* the limit clause,
    or we get an SQL syntax error.
    
    This commit changes this (incorrect):
    
      SELECT  * FROM "USERS" WHERE "USERS"."NAME" = 'admin' FETCH FIRST 10 ROWS ONLY OFFSET 4 ROWS
    
    to this (correct):
    
      SELECT  * FROM "USERS" WHERE "USERS"."NAME" = 'admin' OFFSET 4 ROWS FETCH FIRST 10 ROWS ONLY

--- lib/arel/visitors/derby.rb
+++ lib/arel/visitors/derby.rb
@@ -7,8 +7,8 @@ module Arel
         [
          o.cores.map { |x| visit(x) }.join,
          ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-         (visit(o.limit) if o.limit),
          (visit(o.offset) if o.offset),
+         (visit(o.limit) if o.limit),
          (visit(o.lock) if o.lock),
         ].compact.join ' '
       end
