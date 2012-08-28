c49fbbe9410ef9cce9e5c01f03d3acf9b9afc07f
kares <self@kares.org>
2012-08-28 13:37:48 +0200

    initial Rails 4.0 compatibility ActiveRecord::ConnectionHandling got introduced and is expected to contain the "#{config[:adapter]}_connection" instance method instead of ActiveRecord::Base's singleton class

--- lib/arjdbc/jdbc/connection_methods.rb
+++ lib/arjdbc/jdbc/connection_methods.rb
@@ -1,16 +1,22 @@
-class ActiveRecord::Base
-  class << self
-    def jdbc_connection(config)
-      adapter_class = config[:adapter_class]
-      adapter_class ||= ::ActiveRecord::ConnectionAdapters::JdbcAdapter
-      adapter_class.new(nil, logger, config)
-    end
-    alias jndi_connection jdbc_connection
+module ActiveRecord
+  if defined? ActiveRecord::ConnectionHandling # 4.0
+    ConnectionMethods = ActiveRecord::ConnectionHandling
+  else # 3.x
+    ConnectionMethods = (class << ActiveRecord::Base; self; end)
+  end
+end
 
-    def embedded_driver(config)
-      config[:username] ||= "sa"
-      config[:password] ||= ""
-      jdbc_connection(config)
-    end
+ActiveRecord::ConnectionMethods.module_eval do
+  def jdbc_connection(config)
+    adapter_class = config[:adapter_class]
+    adapter_class ||= ::ActiveRecord::ConnectionAdapters::JdbcAdapter
+    adapter_class.new(nil, logger, config)
+  end
+  alias jndi_connection jdbc_connection
+
+  def embedded_driver(config)
+    config[:username] ||= "sa"
+    config[:password] ||= ""
+    jdbc_connection(config)
   end
 end
--- lib/arjdbc/mssql/connection_methods.rb
+++ lib/arjdbc/mssql/connection_methods.rb
@@ -1,31 +1,29 @@
-class ActiveRecord::Base
-  class << self
-    def mssql_connection(config)
-      require "arjdbc/mssql"
-      config[:host] ||= "localhost"
-      config[:port] ||= 1433
-      config[:driver] ||= "net.sourceforge.jtds.jdbc.Driver"
-      config[:adapter_spec] = ::ArJdbc::MsSQL
+ActiveRecord::ConnectionMethods.module_eval do
+  def mssql_connection(config)
+    require "arjdbc/mssql"
+    config[:host] ||= "localhost"
+    config[:port] ||= 1433
+    config[:driver] ||= "net.sourceforge.jtds.jdbc.Driver"
+    config[:adapter_spec] = ::ArJdbc::MsSQL
 
-      url = "jdbc:jtds:sqlserver://#{config[:host]}:#{config[:port]}/#{config[:database]}"
+    url = "jdbc:jtds:sqlserver://#{config[:host]}:#{config[:port]}/#{config[:database]}"
 
-      # Instance is often a preferrable alternative to port when dynamic ports are used.
-      # If instance is specified then port is essentially ignored.
-      url << ";instance=#{config[:instance]}" if config[:instance]
+    # Instance is often a preferrable alternative to port when dynamic ports are used.
+    # If instance is specified then port is essentially ignored.
+    url << ";instance=#{config[:instance]}" if config[:instance]
 
-      # This will enable windows domain-based authentication and will require the JTDS native libraries be available.
-      url << ";domain=#{config[:domain]}" if config[:domain]
+    # This will enable windows domain-based authentication and will require the JTDS native libraries be available.
+    url << ";domain=#{config[:domain]}" if config[:domain]
 
-      # AppName is shown in sql server as additional information against the connection.
-      url << ";appname=#{config[:appname]}" if config[:appname]
-      config[:url] ||= url
+    # AppName is shown in sql server as additional information against the connection.
+    url << ";appname=#{config[:appname]}" if config[:appname]
+    config[:url] ||= url
 
-      if !config[:domain]
-        config[:username] ||= "sa"
-        config[:password] ||= ""
-      end
-      jdbc_connection(config)
+    if !config[:domain]
+      config[:username] ||= "sa"
+      config[:password] ||= ""
     end
-    alias_method :jdbcmssql_connection, :mssql_connection
+    jdbc_connection(config)
   end
+  alias_method :jdbcmssql_connection, :mssql_connection
 end
