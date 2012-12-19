3a71050b08526688c67903d82d536052ed2595e7
kares <self@kares.org>
2012-12-19 21:30:02 +0100

    run JNDI tests with a pooled data source as well

--- test/jndi_test.rb
+++ test/jndi_test.rb
@@ -2,10 +2,31 @@
 # commons-dbcp, commons-pool, and commons-collections.
 
 require 'jdbc_common'
+
 require 'db/jndi_config'
 
 class DerbyJndiTest < Test::Unit::TestCase
   include SimpleTestMethods
+
+  def self.startup
+    ActiveRecord::Base.establish_connection JNDI_CONFIG
+  end
+
+  test "(raw) connection is a jndi connection" do
+    connection = ActiveRecord::Base.connection.raw_connection
+    assert_true connection.jndi_connection?
+  end
+
+end
+
+require 'db/jndi_pooled_config'
+
+class DerbyJndiPooledTest < Test::Unit::TestCase
+  include SimpleTestMethods
+
+  def self.startup
+    ActiveRecord::Base.establish_connection JNDI_POOLED_CONFIG
+  end
 
   test "(raw) connection is a jndi connection" do
     connection = ActiveRecord::Base.connection.raw_connection
