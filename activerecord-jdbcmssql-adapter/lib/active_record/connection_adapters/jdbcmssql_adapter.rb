0dcf4449bb571188c988f201a048607e12b1c2ec
Arun Agrawal <arunagw@gmail.com>
2012-12-27 11:23:28 +0530

    it should be jtds Closes #283

--- activerecord-jdbcmssql-adapter/lib/active_record/connection_adapters/jdbcmssql_adapter.rb
+++ activerecord-jdbcmssql-adapter/lib/active_record/connection_adapters/jdbcmssql_adapter.rb
@@ -1,4 +1,4 @@
-require 'jdbc/mssql'
+require 'jdbc/jtds'
 # NOTE: the adapter has only support for working with the
 # open-source jTDS driver (won't work with MS's driver) !
 Jdbc::JTDS.load_driver(:require) if Jdbc::JTDS.respond_to?(:load_driver)
