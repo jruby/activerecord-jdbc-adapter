a40c4c0c3160a2a21d3a95ad0c41e3a05b2e7ff6
kares <self@kares.org>
2013-01-08 12:21:58 +0100

    support for setting (custom) jdbc driver properties in config

--- lib/arjdbc/jdbc/driver.rb
+++ lib/arjdbc/jdbc/driver.rb
@@ -1,9 +1,17 @@
 module ActiveRecord
   module ConnectionAdapters
     class JdbcDriver
-      def initialize(name)
+      attr_reader :name, :properties
+      
+      def initialize(name, properties = {})
         @name = name
         @driver = driver_class.new
+        if properties.is_a?(Java::JavaUtil::Properties)
+          @properties = properties # allow programmatically set properties
+        else
+          @properties = Java::JavaUtil::Properties.new
+          properties.each { |key, val| @properties[key] = val.to_s } if properties
+        end
       end
 
       def driver_class
@@ -25,10 +33,10 @@ module ActiveRecord
 
       def connection(url, user, pass)
         # bypass DriverManager to get around problem with dynamically loaded jdbc drivers
-        props = java.util.Properties.new
-        props.setProperty("user", user)
-        props.setProperty("password", pass)
-        @driver.connect(url, props)
+        properties = self.properties.clone
+        properties.setProperty("user", user) if user
+        properties.setProperty("password", pass) if pass
+        @driver.connect(url, properties)
       end
     end
   end
--- test/db/jdbc.rb
+++ test/db/jdbc.rb
@@ -4,10 +4,12 @@ require 'db/mysql_config'
 require 'jdbc/mysql' # driver not loaded for plain JDBC
 Jdbc::MySQL.load_driver
 
-ActiveRecord::Base.establish_connection({
+JDBC_CONFIG = {
   :adapter => 'jdbc',
   :driver => 'com.mysql.jdbc.Driver',
   :url => "jdbc:mysql://#{MYSQL_CONFIG[:host]}/#{MYSQL_CONFIG[:database]}",
   :username => MYSQL_CONFIG[:username],
   :password => MYSQL_CONFIG[:password],
-})
+}
+
+ActiveRecord::Base.establish_connection(JDBC_CONFIG)
