03a830d0bfb0d2dfc54bdf287507d4d24d0b0472
Joshua Schniper <Joshua.Schniper@gmail.com>
2013-01-19 20:33:38 +0100

    [postgres] bug in create_database without options (fixes #306)

--- test/postgres_simple_test.rb
+++ test/postgres_simple_test.rb
@@ -227,3 +227,33 @@ end
 class PostgresHasManyThroughTest < Test::Unit::TestCase
   include HasManyThroughMethods
 end
+
+class PostgresTest < Test::Unit::TestCase
+
+  test 'create_database (with options)' do
+    connection = connection_stub
+    connection.expects(:execute).with '' + 
+      "CREATE DATABASE \"mega_development\" ENCODING='utf8' TABLESPACE = \"TS1\" OWNER = \"kimcom\""
+    connection.create_database 'mega_development', 
+      :tablespace => :'TS1', 'owner' => 'kimcom', :invalid => 'ignored'
+  end
+  
+  test 'create_database (no options)' do
+    connection = connection_stub
+    connection.expects(:execute).with "CREATE DATABASE \"mega_development\" ENCODING='utf8'"
+    connection.create_database 'mega_development'
+  end
+  
+  private
+  
+  def connection_stub
+    connection = mock('connection')
+    (class << connection; self; end).class_eval do
+      def self.alias_chained_method(*args); args; end
+    end
+    def connection.configure_connection; nil; end
+    connection.extend ArJdbc::PostgreSQL
+    connection
+  end
+  
+end
