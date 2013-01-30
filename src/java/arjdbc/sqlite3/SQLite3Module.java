19b2f335cbe643426a8024fe5ec45d0aff7ffd00
kares <self@kares.org>
2013-01-30 14:54:43 +0100

    missing license for SQLite3Module.java

--- src/java/arjdbc/sqlite3/SQLite3Module.java
+++ src/java/arjdbc/sqlite3/SQLite3Module.java
@@ -1,6 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2013 Karol Bucek.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package arjdbc.sqlite3;
 
@@ -14,12 +33,13 @@ import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.util.ByteList;
 
 /**
- *
+ * ArJdbc::SQLite3
+ * 
  * @author kares
  */
 public class SQLite3Module {
     
-    public static void load(RubyModule arJdbc) {
+    public static void load(final RubyModule arJdbc) {
         RubyModule sqlite3 = arJdbc.defineModuleUnder("SQLite3");
         sqlite3.defineAnnotatedMethods( SQLite3Module.class );
     }
