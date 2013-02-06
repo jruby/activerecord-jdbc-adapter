73f328790d47bf59139763b6a6d46900173ed542
kares <self@kares.org>
2013-02-06 19:56:12 +0100

    follow Column-naming convention in MySQL adapter ArJdbc module
    Conflicts:
    	lib/arjdbc/mysql/adapter.rb

--- lib/arjdbc/mysql/adapter.rb
+++ lib/arjdbc/mysql/adapter.rb
@@ -14,14 +14,15 @@ module ::ArJdbc
     end
 
     def self.column_selector
-      [ /mysql/i, lambda { |_,column| column.extend(::ArJdbc::MySQL::ColumnExtensions) } ]
+      [ /mysql/i, lambda { |_,column| column.extend(::ArJdbc::MySQL::Column) } ]
     end
 
     def self.jdbc_connection_class
       ::ActiveRecord::ConnectionAdapters::MySQLJdbcConnection
     end
 
-    module ColumnExtensions
+    module Column
+      
       def extract_default(default)
         if sql_type =~ /blob/i || type == :text
           if default.blank?
@@ -87,8 +88,11 @@ module ::ArJdbc
       def missing_default_forged_as_empty_string?(default)
         type != :string && !null && default == ''
       end
+      
     end
 
+    ColumnExtensions = Column # :nodoc: backwards-compatibility
+    
     def modify_types(types)
       types[:primary_key] = "int(11) DEFAULT NULL auto_increment PRIMARY KEY"
       types[:integer] = { :name => 'int', :limit => 4 }
@@ -465,7 +469,7 @@ module ActiveRecord
     remove_const(:MysqlAdapter) if const_defined?(:MysqlAdapter)
 
     class MysqlColumn < JdbcColumn
-      include ::ArJdbc::MySQL::ColumnExtensions
+      include ::ArJdbc::MySQL::Column
 
       def initialize(name, *args)
         if Hash === name
