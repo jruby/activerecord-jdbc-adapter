4643f35acab708f45fefd83e09d907a7210a9c72
kares <self@kares.org>
2013-02-07 11:41:56 +0100

    derby (native) types review & cleanup, externalize AR patch-ing

--- test/derby_simple_test.rb
+++ test/derby_simple_test.rb
@@ -1,5 +1,6 @@
 # encoding: ASCII-8BIT
 require 'jdbc_common'
+require 'db/derby'
 
 class DerbySimpleTest < Test::Unit::TestCase
   include SimpleTestMethods
@@ -17,7 +18,7 @@ class DerbySimpleTest < Test::Unit::TestCase
   end
 
   def test_find_with_include_and_order
-    users = User.find(:all, :include=>[:entries], :order=>"entries.rating DESC", :limit=>2)
+    users = User.find(:all, :include=>[:entries], :order=>"entries.rating DESC", :limit => 2)
 
     assert users.include?(@user)
   end
@@ -95,26 +96,28 @@ class DerbySimpleTest < Test::Unit::TestCase
   end
 
   def test_data_types
-    # From test/models/data_types.rb, with the modifications as noted in the comments.
+    # from test/models/data_types.rb, with the modifications as noted in the comments.
     expected_types = [
       ["id",                          :integer,   { }],
-      ["sample_timestamp",            :datetime,  { }],   # :timestamp is just an alias for :datetime in Derby
+      ["sample_timestamp",            :datetime,  { }], # :timestamp is just an alias for :datetime in Derby
       ["sample_datetime",             :datetime,  { }],
       ["sample_date",                 :date,      { }],
       ["sample_time",                 :time,      { }],
-      ["sample_decimal",              :integer,   { :precision => 15, :scale => 0 }],   # it's an :integer because the :scale is 0 (...right?)
-      ["sample_small_decimal",        :decimal,   { :precision => 3,  :scale => 2 }],
-      ["sample_default_decimal",      :integer,   { }],                 # decimal and integer are the same type in Derby
+      # NOTE: it's an :integer because the :scale is 0 (...right?) :
+      ["sample_decimal",              :integer,   { :precision => 9, :scale => 0 }],
+      ["sample_small_decimal",        :decimal,   { :precision => 3, :scale => 2, :default => 3.14 }],
+      ["sample_default_decimal",      :integer,   { }], # decimal by default assumes :scale => 0
       ["sample_float",                :float,     { }],
       ["sample_binary",               :binary,    { }],
       ["sample_boolean",              :boolean,   { }],
       ["sample_string",               :string,    { :default => '' }],
-      ["sample_integer",              :integer,   { }],                 # don't care about the limit
-      ["sample_integer_with_limit_2", :integer,   { }],                 # don't care about the limit
-      ["sample_integer_with_limit_8", :integer,   { }],                 # don't care about the limit
+      ["sample_integer",              :integer,   { }], # don't care about the limit
+      ["sample_integer_with_limit_2", :integer,   { }], # don't care about the limit
+      ["sample_integer_with_limit_8", :integer,   { }], # don't care about the limit
       ["sample_integer_no_limit",     :integer,   { }],
       ["sample_integer_neg_default",  :integer,   { :default => -1 }],
       ["sample_text",                 :text,      { }],
+      ["big_decimal",                 :integer,   { :precision => 31, :scale => 0 }],
     ].sort{|a,b| a[0] <=> b[0]}
 
     column_names = (expected_types.map{|et| et[0]} + DbType.column_names).sort.uniq
@@ -133,6 +136,10 @@ class DerbySimpleTest < Test::Unit::TestCase
 
     assert_equal expected_types, result
   end
+end
+
+class DerbyMultibyteTest < Test::Unit::TestCase
+  include MultibyteTestMethods
 end
 
 class DerbyHasManyThroughTest < Test::Unit::TestCase
