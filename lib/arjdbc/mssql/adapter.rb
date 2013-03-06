0e3f7dc881840933e69cb9706feb25a9eaaed7a3
kares <self@kares.org>
2013-03-06 09:40:09 +0100

    [mssql] execute_procedure support (AR-SQLServer style - closes #266)

--- lib/arjdbc/mssql/adapter.rb
+++ lib/arjdbc/mssql/adapter.rb
@@ -540,6 +540,25 @@ module ArJdbc
       "#{table_name}.#{(primary_column || columns.first).name}"
     end
     
+    def exec_proc(proc_name, *variables)
+      vars = 
+        if variables.any? && variables.first.is_a?(Hash)
+          variables.first.map { |k, v| "@#{k} = #{quote(v)}" }
+        else
+          variables.map { |v| quote(v) }
+        end.join(', ')
+      sql = "EXEC #{proc_name} #{vars}".strip
+      log(sql, 'Execute Procedure') do
+        result = @connection.execute_query(sql)
+        result.map do |row| 
+          row = row.is_a?(Hash) ? row.with_indifferent_access : row
+          yield(row) if block_given?
+          row
+        end
+      end
+    end
+    alias_method :execute_procedure, :exec_proc # AR-SQLServer-Adapter naming
+    
     private
     
     def _execute(sql, name = nil)
