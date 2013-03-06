bc3c63f9627725b4275bbd124caf475046f98ffd
kares <self@kares.org>
2013-03-06 11:48:33 +0100

    rename table / remove column (with) index tests for sqlserver

--- test/db/mssql/simple_test.rb
+++ test/db/mssql/simple_test.rb
@@ -64,6 +64,36 @@ class MSSQLSimpleTest < Test::Unit::TestCase
     assert_not_nil ActiveRecord::Base.connection.charset
   end
   
+  def test_rename_table
+    user = User.create! :login => 'luser'
+    begin
+      ActiveRecord::Base.connection.rename_table 'users', 'lusers'
+      lusers = Class.new(ActiveRecord::Base)
+      lusers.table_name = 'lusers'
+      assert_kind_of ActiveRecord::Base, lusers.find(user.id)
+    ensure
+      CreateUsers.up rescue nil
+      ActiveRecord::Base.connection.execute("DROP TABLE lusers") rescue nil
+    end
+  end
+  
+  def test_remove_column_with_index
+    ActiveRecord::Schema.define do
+      add_column :entries, 'another_column', :string
+      add_index :entries, 'another_column'
+    end
+
+    columns = ActiveRecord::Base.connection.columns("entries")
+    assert columns.find { |col| col.name == 'another_column' }
+
+    ActiveRecord::Schema.define do
+      remove_column "entries", 'another_column'
+    end
+
+    columns = ActiveRecord::Base.connection.columns("entries")
+    assert ! columns.find { |col| col.name == 'another_column' }
+  end
+  
   def test_find_by_sql_WITH_statement
     user = User.create! :login => 'ferko'
     Entry.create! :title => 'aaa', :user_id => user.id
