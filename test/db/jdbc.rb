4fac0789973803614b7252068fc31f0f03f20f31
kares <self@kares.org>
2013-03-07 10:58:30 +0100

    explicit MySQL default port no if missing in (test) config for JDBC URL

--- test/db/jdbc.rb
+++ test/db/jdbc.rb
@@ -7,9 +7,10 @@ Jdbc::MySQL.load_driver
 JDBC_CONFIG = {
   :adapter => 'jdbc',
   :driver => 'com.mysql.jdbc.Driver',
-  :url => "jdbc:mysql://#{MYSQL_CONFIG[:host]}/#{MYSQL_CONFIG[:database]}",
   :username => MYSQL_CONFIG[:username],
   :password => MYSQL_CONFIG[:password],
 }
+JDBC_CONFIG[:url] = "jdbc:mysql://" << 
+  "#{MYSQL_CONFIG[:host]}:#{MYSQL_CONFIG[:port] || 3306}/#{MYSQL_CONFIG[:database]}"
 
 ActiveRecord::Base.establish_connection(JDBC_CONFIG)
