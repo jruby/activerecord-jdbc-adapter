30ec4e408e7acd0fb9c025f6fc0e2088ed00fc2c
kares <self@kares.org>
2013-03-21 09:15:18 +0100

    test white-space in generated CREATE_DATABASE SQL

--- test/db/postgres/unit_test.rb
+++ test/db/postgres/unit_test.rb
@@ -5,14 +5,14 @@ class PostgresUnitTest < Test::Unit::TestCase
   test 'create_database (with options)' do
     connection = connection_stub
     connection.expects(:execute).with '' + 
-      "CREATE DATABASE \"mega_development\" ENCODING='utf8' TABLESPACE = \"TS1\" OWNER = \"kimcom\""
+      "CREATE DATABASE \"mega_development\" ENCODING = 'utf8' TABLESPACE = \"TS1\" OWNER = \"kimcom\""
     connection.create_database 'mega_development', 
       :tablespace => :'TS1', 'owner' => 'kimcom', :invalid => 'ignored'
   end
   
   test 'create_database (no options)' do
     connection = connection_stub
-    connection.expects(:execute).with "CREATE DATABASE \"mega_development\" ENCODING='utf8'"
+    connection.expects(:execute).with "CREATE DATABASE \"mega_development\" ENCODING = 'utf8'"
     connection.create_database 'mega_development'
   end
   
