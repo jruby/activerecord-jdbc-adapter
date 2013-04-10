1d9addd5033b6ede25acd78721a96fcdbbc2431b
kares <self@kares.org>
2013-04-10 08:34:57 +0200

    cleanup - merge core_ext with base_ext + use method_defined? due 1.9 ... since instance_methods returns strings on 1.8 but symbol names on 1.9

--- lib/arjdbc/jdbc/base_ext.rb
+++ lib/arjdbc/jdbc/base_ext.rb
@@ -1,15 +1,40 @@
 module ActiveRecord
-  class Base  # reopen
+  # ActiveRecord::Base extensions.
+  Base.class_eval do
     class << self
-      # Allow adapters to provide their own reset_column_information methods
-      #
-      # NOTE: This only affects the current thread's connection.
-      def reset_column_information_with_arjdbc_base_ext
-        # Invoke the adapter-specific reset_column_information method
+      # Allow adapters to provide their own {#reset_column_information} method.
+      # @note This only affects the current thread's connection.
+      def reset_column_information_with_arjdbc
+        # invoke the adapter-specific reset_column_information method
         connection.reset_column_information if connection.respond_to?(:reset_column_information)
-        reset_column_information_without_arjdbc_base_ext
+        reset_column_information_without_arjdbc
       end
-      alias_method_chain :reset_column_information, :arjdbc_base_ext unless instance_methods.include?("reset_column_information_without_arjdbc_base_ext")
+      unless method_defined?("reset_column_information_without_arjdbc")
+        alias_method_chain :reset_column_information, :arjdbc
+      end
+    end
+  end
+  
+  # Represents exceptions that have propagated up through the JDBC API.
+  class JDBCError < ActiveRecordError
+    # The vendor code or error number that came from the database
+    attr_accessor :errno
+    # The full Java SQLException object that was raised.
+    attr_accessor :sql_exception
+  end
+
+  module ConnectionAdapters # :nodoc:
+    # Allows properly re-defining methods that may already be alias_method_chain'd.
+    module ShadowCoreMethods
+      def alias_chained_method(name, feature, target)
+        # NOTE: aliasing for things such as columns (with feature query_cache)
+        # seems to only be used for 2.3 since 3.0 methods are not aliased ...
+        if method_defined?(method = "#{name}_without_#{feature}")
+          alias_method method, target
+        else
+          alias_method name, target if name.to_s != target.to_s
+        end
+      end
     end
   end
 end
