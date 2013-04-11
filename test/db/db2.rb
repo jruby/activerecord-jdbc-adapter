e778aeffc47087b0bb99ca4c41fb66d1be6862ca
kares <self@kares.org>
2013-04-11 12:14:56 +0200

    try to enable testing DB2 adapter against AS400 using IBM's JTOpen driver

--- test/db/db2.rb
+++ test/db/db2.rb
@@ -9,24 +9,31 @@ config[:port] = ENV['DB2PORT'] if ENV['DB2PORT']
 config[:username] = ENV['DB2USER'] if ENV['DB2USER'] # db2inst1
 config[:password] = ENV['DB2PASS'] if ENV['DB2PASS']
 
+# for AS400 specify the full JDBC URL "jdbc:as400://..."
+config[:url] = ENV['DB2URL'] if ENV['DB2URL']
+
 # DB2 does not like "_" in database names :
 # SQL0104N  An unexpected token "weblog_development" was found
 # create the sample database using `db2sampl` command
-database = ENV['DB2DATABASE'] || 'SAMPLE'
-if config[:host]
-  config[:database] = database
-else
-  config[:url] = "jdbc:db2:#{database}" # local instance
+unless config[:url]
+  database = ENV['DB2DATABASE'] || 'SAMPLE'
+  if config[:host]
+    config[:database] = database
+  else
+    config[:url] = "jdbc:db2:#{database}" # local instance
+  end
 end
 
 require 'jdbc/db2'
 # Download IBM DB2 JCC driver from :
 # https://www-304.ibm.com/support/docview.wss?rs=4020&uid=swg21385217
+# or http://sourceforge.net/projects/jt400/ for AS400
+jdbc_db2 = (config[:url] || '') =~ /\:as400/ ? Jdbc::AS400 : Jdbc::DB2
 begin
-  Java::JavaClass.for_name(Jdbc::DB2.driver_name)
+  Java::JavaClass.for_name(jdbc_db2.driver_name)
 rescue NameError
   begin
-    Jdbc::DB2.load_driver
+    jdbc_db2.load_driver
   rescue LoadError => e
     puts "Please setup a JDBC driver to run the DB2 tests !"
     raise e
