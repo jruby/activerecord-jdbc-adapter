05c97fd2c620ab1e40d7ebb5845f59327634f73e
kares <self@kares.org>
2013-04-24 10:07:39 +0200

    these comments are no longer relevant as we try to lazy load the holy 3

--- activerecord-jdbcsqlite3-adapter/lib/activerecord-jdbcsqlite3-adapter.rb
+++ activerecord-jdbcsqlite3-adapter/lib/activerecord-jdbcsqlite3-adapter.rb
@@ -1,5 +1,3 @@
 # NOTE: here for Bundler to auto-load the gem unless :require => false
-require 'arjdbc' 
-# ArJdbc loads arjdbc/sqlite3 eagerly to override Rails' native adapter thus 
-# this will work even if `adapter: sqlite3` specified - but just in case :
+require 'arjdbc'
 require 'arjdbc/sqlite3'
