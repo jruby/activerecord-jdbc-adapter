9020abeb6bb9b50bd3c947ee4a3dac89cb59526e
kares <self@kares.org>
2013-05-13 13:17:09 +0200

    support for wrapping exception's with JDBCError

--- lib/arjdbc/jdbc/base_ext.rb
+++ lib/arjdbc/jdbc/base_ext.rb
@@ -4,7 +4,7 @@ module ActiveRecord
     class << self
       # Allow adapters to provide their own {#reset_column_information} method.
       # @note This only affects the current thread's connection.
-      def reset_column_information_with_arjdbc
+      def reset_column_information_with_arjdbc # :nodoc:
         # invoke the adapter-specific reset_column_information method
         connection.reset_column_information if connection.respond_to?(:reset_column_information)
         reset_column_information_without_arjdbc
@@ -17,14 +17,36 @@ module ActiveRecord
   
   # Represents exceptions that have propagated up through the JDBC API.
   class JDBCError < ActiveRecordError
-    # The vendor code or error number that came from the database
+    # The vendor code or error number that came from the database.
+    # @note writer being used by the Java API
     attr_accessor :errno
     # The full Java SQLException object that was raised.
+    # @note writer being used by the Java API
     attr_accessor :sql_exception
+    
+    attr_reader :original_exception, :raw_backtrace
+    
+    def initialize(message = nil, original_exception = nil) # $!
+      super(message)
+      @original_exception = original_exception
+    end
+
+    def set_backtrace(backtrace)
+      @raw_backtrace = backtrace
+      if nested = original_exception
+        backtrace = backtrace - ( 
+          nested.respond_to?(:raw_backtrace) ? nested.raw_backtrace : nested.backtrace )
+        backtrace << "#{nested.backtrace.first}: #{nested.message} (#{nested.class.name})"
+        backtrace += nested.backtrace[1..-1] || []
+      end
+      super(backtrace)
+    end
+    
   end
 
   module ConnectionAdapters # :nodoc:
-    # Allows properly re-defining methods that may already be alias_method_chain'd.
+    # Allows properly re-defining methods that may already be alias-chain-ed.
+    # Query caching works even with overriden alias_method_chain'd methods.
     module ShadowCoreMethods
       def alias_chained_method(name, feature, target)
         # NOTE: aliasing for things such as columns (with feature query_cache)
