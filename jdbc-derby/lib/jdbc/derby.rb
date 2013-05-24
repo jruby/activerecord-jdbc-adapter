290ff88c730ef4e915cb45c4cc9ee11233b501d1
Benjamin Fleischer <github@benjaminfleischer.com>
2013-05-24 10:00:14 -0500

    Fix jdbc-derby warning when gemspec read by non-jruby

--- jdbc-derby/lib/jdbc/derby.rb
+++ jdbc-derby/lib/jdbc/derby.rb
@@ -1,9 +1,8 @@
 warn "Jdbc-Derby is only for use with JRuby" if (JRUBY_VERSION.nil? rescue true)
+require 'jdbc/derby/version'
 
 module Jdbc
   module Derby
-    DRIVER_VERSION = "10.9.1.0"
-    VERSION = DRIVER_VERSION + ''
 
     def self.driver_jar
       "derby-#{DRIVER_VERSION}.jar"
@@ -18,7 +17,7 @@ module Jdbc
     end
 
     if defined?(JRUBY_VERSION) && # enable backwards-compat behavior :
-      ( Java::JavaLang::Boolean.get_boolean("jdbc.driver.autoload") || 
+      ( Java::JavaLang::Boolean.get_boolean("jdbc.driver.autoload") ||
         Java::JavaLang::Boolean.get_boolean("jdbc.derby.autoload") )
       warn "autoloading JDBC driver on require 'jdbc/derby'" if $VERBOSE
       load_driver :require
