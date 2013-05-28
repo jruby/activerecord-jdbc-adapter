ce130a79dcd717e3dfec027175dd01dbc641191d
Pierrick Rouxel <pierrick.rouxel@me.com>
2013-05-28 16:58:45 +0200

    fix deprecated warning with rake tasks

--- lib/arjdbc/railtie.rb
+++ lib/arjdbc/railtie.rb
@@ -3,7 +3,7 @@ require 'rails/railtie'
 module ArJdbc
   class Railtie < ::Rails::Railtie
     rake_tasks do
-      load File.expand_path('jdbc/rake_tasks.rb', File.dirname(__FILE__))
+      load File.expand_path('tasks.rb', File.dirname(__FILE__))
     end
   end
 end
