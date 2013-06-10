c39bab792551050d53ea1f66e8890ac6d7efa4db
kares <self@kares.org>
2013-06-10 14:32:19 +0200

    3.7.15-SNAPSHOT from 2012-12-16 seems to be working fine, source : https://bitbucket.org/xerial/sqlite-jdbc/downloads
    NOTE: sqlite-jdbc-3.7.15-SNAPSHOT-2.jar has issues passing our tests (NPE)

--- jdbc-sqlite3/lib/jdbc/sqlite3/version.rb
+++ jdbc-sqlite3/lib/jdbc/sqlite3/version.rb
@@ -1,6 +1,6 @@
 module Jdbc
   module SQLite3
-    DRIVER_VERSION = '3.7.2'
-    VERSION = DRIVER_VERSION + '.1'
+    DRIVER_VERSION = '3.7.15.SNAPSHOT'
+    VERSION = DRIVER_VERSION + ''
   end
 end
