dc490826552bf7392930996eda4f171f0636509b
kares <self@kares.org>
2013-06-17 13:36:37 +0200

    allow testing with FireBird

---
+++ test/db/firebird.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+# FireBird setup (Debian/Ubuntu) :
+# sudo dpkg-reconfigure firebird2.5-super
+# - check your SYSDBA password at :
+# sudo cat /etc/firebord/2.5/SYSDBA.password
+# - create a database, run `isql-fb` and :
+#
+# CREATE DATABASE './test.fdb'
+# USER 'SYSDBA' PASSWORD 'masterkey'
+# DEFAULT CHARACTER SET UTF8 ;
+#
+
+config = { :adapter => 'firebird' }
+config[:username] = ENV['FB_USER'] || 'sysdba'
+config[:password] = ENV['FB_PASS'] || 'masterkey'
+config[:database] = ENV['FB_DATABASE'] || './test.fbd'
+
+ActiveRecord::Base.establish_connection(config)
