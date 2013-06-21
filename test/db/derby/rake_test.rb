934577fdde736ed9ced90373037ef6951d44b73d
kares <self@kares.org>
2013-06-21 08:07:21 +0200

    avoid "warning: Ambiguous first argument; make sure." (JRuby 1.6.8)

--- test/db/derby/rake_test.rb
+++ test/db/derby/rake_test.rb
@@ -3,9 +3,9 @@ require 'db/derby'
 
 class DerbyRakeTest < Test::Unit::TestCase
   include RakeTestSupport
-  
+
   def db_name; 'test-rake.derby'; end
-  
+
   def do_teardown
     drop_rake_test_database(:silence)
   end
@@ -17,42 +17,42 @@ class DerbyRakeTest < Test::Unit::TestCase
 
   test 'rake db:drop' do
     create_rake_test_database
-    
+
     Rake::Task["db:drop"].invoke
     assert_false File.directory?('test-rake.derby')
   end
-  
+
   test 'rake db:test:purge' do
     ActiveRecord::Base.connection.disconnect!
     # Rake::Task["db:create"].invoke
     create_rake_test_database do |connection|
       connection.create_table('users') { |t| t.string :name }
     end
-    
+
     Rake::Task["db:test:purge"].invoke
-    
+
     ActiveRecord::Base.establish_connection db_config.merge :database => db_name
     assert_false ActiveRecord::Base.connection.table_exists?('users')
     ActiveRecord::Base.connection.disconnect!
   end
-  
+
   test 'rake db:structure:dump (and db:structure:load)' do
-    # NOTE: need to change db name esp. when run with other (rake) tests 
+    # NOTE: need to change db name esp. when run with other (rake) tests
     # since Derby can not handle too much create/rm - gets inconsistent
     db_name = @db_name = 'test-dump-rake.derby'
     create_rake_test_database(db_name) do |connection|
       create_schema_migrations_table(connection)
       connection.create_table('users') { |t| t.string :name; t.timestamps }
     end
-    
+
     structure_sql = File.join('db', structure_sql_filename)
     begin
       Dir.mkdir 'db' # db/structure.sql
       Rake::Task["db:structure:dump"].invoke
-      
+
       assert File.exists?(structure_sql)
-      assert_match /CREATE TABLE "?USERS"?/i, File.read(structure_sql)
-      
+      assert_match(/CREATE TABLE "?USERS"?/i, File.read(structure_sql))
+
       # db:structure:load
       ActiveRecord::Base.connection.disconnect!
       # drop_rake_test_database(:silence)
@@ -60,9 +60,9 @@ class DerbyRakeTest < Test::Unit::TestCase
       ActiveRecord::Base.connection.drop_table('users')
       ActiveRecord::Base.connection.drop_table('schema_migrations')
       ActiveRecord::Base.connection.disconnect!
-      
+
       Rake::Task["db:structure:load"].invoke
-      
+
       ActiveRecord::Base.establish_connection db_config.merge :database => db_name
       assert ActiveRecord::Base.connection.table_exists?('users')
       ActiveRecord::Base.connection.disconnect!
@@ -71,12 +71,12 @@ class DerbyRakeTest < Test::Unit::TestCase
       Dir.rmdir 'db'
     end
   end
-  
+
 #  test 'rake db:charset' do
 #    expect_rake_output('UTF-8')
 #    Rake::Task["db:charset"].invoke
 #  end
-  
+
   def create_rake_test_database(db_name = self.db_name)
     ActiveRecord::Base.establish_connection db_config.merge :database => db_name
     if block_given?
@@ -84,7 +84,7 @@ class DerbyRakeTest < Test::Unit::TestCase
     end
     ActiveRecord::Base.connection.disconnect!
   end
-  
+
   def drop_rake_test_database(silence = nil)
     if silence
       FileUtils.rm_rf(@db_name) if File.exists?(@db_name)
@@ -92,5 +92,5 @@ class DerbyRakeTest < Test::Unit::TestCase
       FileUtils.rm_rf(@db_name)
     end
   end
-  
+
 end
--- test/db/sqlite3/rake_test.rb
+++ test/db/sqlite3/rake_test.rb
@@ -3,13 +3,13 @@ require 'db/sqlite3'
 
 class SQLite3RakeTest < Test::Unit::TestCase
   include RakeTestSupport
-  
+
   def db_name; 'rake_test.sqlite3'; end
-  
+
   def do_setup
     File.delete('rake_test.sqlite3') if File.exists?('rake_test.sqlite3')
   end
-  
+
   def do_teardown
     File.delete('rake_test.sqlite3') if File.exists?('rake_test.sqlite3')
   end
@@ -17,43 +17,43 @@ class SQLite3RakeTest < Test::Unit::TestCase
   test 'rake db:create (and db:drop)' do
     Rake::Task["db:create"].invoke
     assert_true File.exists?('rake_test.sqlite3')
-    
+
     Rake::Task["db:drop"].invoke
     assert_false File.exists?('rake_test.sqlite3')
   end
-  
+
   test 'rake db:test:purge' do
     # Rake::Task["db:create"].invoke
     ActiveRecord::Base.establish_connection db_config.merge :database => db_name
     ActiveRecord::Base.connection.create_table('users') { |t| t.string :name }
     ActiveRecord::Base.connection.disconnect!
-    
+
     Rake::Task["db:test:purge"].invoke
-    
+
     ActiveRecord::Base.establish_connection db_config.merge :database => db_name
     assert_false ActiveRecord::Base.connection.table_exists?('users')
     ActiveRecord::Base.connection.disconnect!
   end
-  
+
   test 'rake db:structure:dump (and db:structure:load)' do
     omit('sqlite3 not available') unless self.class.find_executable?('sqlite3')
     create_rake_test_database do |connection|
       create_schema_migrations_table(connection)
       connection.create_table('users') { |t| t.string :name; t.timestamps }
     end
-    
+
     structure_sql = File.join('db', structure_sql_filename)
     begin
       Dir.mkdir 'db' # db/structure.sql
       Rake::Task["db:structure:dump"].invoke
-      
+
       assert File.exists?(structure_sql)
-      assert_match /CREATE TABLE .*?users/, File.read(structure_sql)
-      
+      assert_match(/CREATE TABLE .*?users/, File.read(structure_sql))
+
       # db:structure:load
       File.delete('rake_test.sqlite3')
       Rake::Task["db:structure:load"].invoke
-      
+
       ActiveRecord::Base.establish_connection db_config.merge :database => db_name
       assert ActiveRecord::Base.connection.table_exists?('users')
       ActiveRecord::Base.connection.disconnect!
@@ -62,10 +62,10 @@ class SQLite3RakeTest < Test::Unit::TestCase
       Dir.rmdir 'db'
     end
   end
-  
+
   test 'rake db:charset' do
     expect_rake_output('UTF-8')
     Rake::Task["db:charset"].invoke
   end
-  
+
 end
