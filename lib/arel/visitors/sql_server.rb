e19160e296b39ea6e8c31feb7e30e100d25ea18e
kares <self@kares.org>
2013-07-16 15:29:53 +0200

    make our SQLServer arel visitor extend the MSSQL built-in one if available

--- lib/arel/visitors/sql_server.rb
+++ lib/arel/visitors/sql_server.rb
@@ -2,39 +2,46 @@ require 'arel/visitors/compat'
 
 module Arel
   module Visitors
-    # @note AREL set's up `Visitors::MSSQL` but we should not use that one !
-    class SQLServer < ToSql
-
-      # Need to mimic the subquery logic in ARel 1.x for select count with limit
-      # See arel/engines/sql/compilers/mssql_compiler.rb for details
+    # @note AREL set's up `Arel::Visitors::MSSQL` but we should not use that one !
+    class SQLServer < const_defined?(:MSSQL) ? MSSQL : ToSql
+      
       def visit_Arel_Nodes_SelectStatement(*args) # [o] AR <= 4.0 [o, a] on 4.1
         o, a = args.first, args.last
 
-        if ! o.limit && o.offset
+        if ! o.limit && ! o.offset
+          return super
+        elsif ! o.limit && o.offset
           raise ActiveRecord::ActiveRecordError, "You must specify :limit with :offset."
         end
 
         order = "ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?
-        if o.limit
-          if select_count?(o)
-            subquery = true
-            sql = o.cores.map do |x|
-              x = x.dup
-              x.projections = [Arel::Nodes::SqlLiteral.new("*")]
-              do_visit_select_core(x, a)
-            end.join
-          else
-            sql = o.cores.map { |x| do_visit_select_core(x, a) }.join
-          end
 
-          order ||= "ORDER BY #{@connection.determine_order_clause(sql)}"
-          replace_limit_offset!(sql, limit_for(o.limit).to_i, o.offset && o.offset.value.to_i, order)
-          sql = "SELECT COUNT(*) AS count_id FROM (#{sql}) AS subquery" if subquery
+        if select_count?(o)
+          subquery = true
+          sql = o.cores.map do |x|
+            x = x.dup
+            x.projections = [Arel::Nodes::SqlLiteral.new("*")]
+            do_visit_select_core(x, a)
+          end.join
         else
-          sql = super
+          sql = o.cores.map { |x| do_visit_select_core(x, a) }.join
         end
+        order ||= "ORDER BY #{@connection.determine_order_clause(sql)}"
+
+        replace_limit_offset!(sql, limit_for(o.limit).to_i, o.offset && o.offset.value.to_i, order)
+
+        sql = "SELECT COUNT(*) AS count_id FROM (#{sql}) AS subquery" if subquery
+
         add_lock!(sql, :lock => o.lock && true)
+
         sql
+      end
+
+      def visit_Arel_Nodes_UpdateStatement o
+        if o.orders.any? && o.limit.nil?
+          o.limit = Nodes::Limit.new(9223372036854775807)
+        end
+        super
       end
 
       # MS-SQL doesn't support "SELECT...FOR UPDATE".  Instead, it needs
@@ -43,13 +50,6 @@ module Arel
       # So, we return nothing here and add the appropriate stuff using add_lock! above.
       def visit_Arel_Nodes_Lock o
         # visit o.expr
-      end
-
-      def visit_Arel_Nodes_UpdateStatement o
-        if o.orders.any? && o.limit.nil?
-          o.limit = Nodes::Limit.new(9223372036854775807)
-        end
-        super
       end
 
       # `top` wouldn't really work here. I.e. User.select("distinct first_name").limit(10) would generate
