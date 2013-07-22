a70dba69afdc2c625799e4d0eebcc07af2b279e8
kares <self@kares.org>
2013-07-22 12:14:51 +0200

    filter SerializedAttributesHelper + the Util module

--- src/java/arjdbc/ArJdbcModule.java
+++ src/java/arjdbc/ArJdbcModule.java
@@ -40,11 +40,11 @@ import org.jruby.runtime.builtin.IRubyObject;
 
 /**
  * ::ArJdbc
- * 
+ *
  * @author kares
  */
 public class ArJdbcModule {
-    
+
     public static RubyModule load(final Ruby runtime) {
         final RubyModule arJdbc = runtime.getOrCreateModule("ArJdbc");
         arJdbc.defineAnnotatedMethods( ArJdbcModule.class );
@@ -52,9 +52,9 @@ public class ArJdbcModule {
     }
 
     /**
-     * Load the Java parts for the given adapter spec module, e.g. to load 
+     * Load the Java parts for the given adapter spec module, e.g. to load
      * ArJdbc::MySQL's Java part: <code>ArJdbc.load_java_part :MySQL</code>
-     * 
+     *
      * NOTE: this method is not intended to be called twice for a given adapter !
      * @param context
      * @param self
@@ -62,15 +62,15 @@ public class ArJdbcModule {
      * @return true
      */
     @JRubyMethod(name = "load_java_part", meta = true, required = 1, optional = 2)
-    public static IRubyObject load_java_part(final ThreadContext context, 
+    public static IRubyObject load_java_part(final ThreadContext context,
         final IRubyObject self, final IRubyObject[] args) {
-        
+
         String connectionClass = args.length > 1 ? args[1].toString() : null;
         String moduleClass = args.length > 2 ? args[2].toString() : null;
-        
+
         final String moduleName = args[0].toString(); // e.g. 'MySQL'
         final String packagePrefix = "arjdbc." + moduleName.toLowerCase() + "."; // arjdbc.mysql
-        
+
         // NOTE: due previous (backwards compatible) conventions there are
         // 2 things we load, the adapter spec module's Java implemented methods
         // and a custom JdbcConnection class (both are actually optional) e.g. :
@@ -78,7 +78,7 @@ public class ArJdbcModule {
         //   MySQLModule.load(RubyModule); // 'arjdbc.mysql' package is assumed
         //   MySQLRubyJdbcConnection.createMySQLJdbcConnectionClass(Ruby, RubyClass);
         //
-        
+
         String connectionClass2 = null;
         if (connectionClass == null) {
              // 'arjdbc.mysql.' + 'MySQL' + 'RubyJdbcConnection'
@@ -89,10 +89,10 @@ public class ArJdbcModule {
              // 'arjdbc.mysql.' + 'MySQL' + 'Module'
             moduleClass = packagePrefix + moduleName + "Module";
         }
-        
+
         final Ruby runtime = context.getRuntime();
         final RubyModule arJdbc = runtime.getModule("ArJdbc");
-        
+
         try {
             final Class<?> module = Class.forName(moduleClass);
              // MySQLModule.load( arJdbc ) :
@@ -108,7 +108,7 @@ public class ArJdbcModule {
         catch (InvocationTargetException e) {
             throw newNativeException(runtime, e);
         }
-        
+
         try {
             Class<?> connection = null;
             try {
@@ -136,10 +136,10 @@ public class ArJdbcModule {
         catch (InvocationTargetException e) {
             throw newNativeException(runtime, e);
         }
-        
+
         return runtime.getTrue();
     }
-    
+
     /**
      * <code>ArJdbc.modules</code>
      * @param context
@@ -150,26 +150,29 @@ public class ArJdbcModule {
     public static IRubyObject modules(final ThreadContext context, final IRubyObject self) {
         final Ruby runtime = context.getRuntime();
         final RubyModule arJdbc = (RubyModule) self;
-        
+
         final Collection<String> constants = arJdbc.getConstantNames();
         final RubyArray modules = runtime.newArray( constants.size() );
-        
+
         for ( final String name : constants ) {
            IRubyObject value = arJdbc.getConstant(name, false);
            // isModule: return false for Ruby Classes
            if ( value != null && value.isModule() ) {
                if ( "MissingFunctionalityHelper".equals(name) ) continue;
+               if ( "SerializedAttributesHelper".equals(name) ) continue;
+               if ( "QuotedPrimaryKeyExtension".equals(name) ) continue;
+               if ( "Util".equals(name) ) continue;
                if ( "Version".equals(name) ) continue;
                modules.append(value);
            }
         }
         return modules;
     }
-    
+
     private static RaiseException newNativeException(final Ruby runtime, final Throwable cause) {
         RubyClass nativeClass = runtime.getClass(NativeException.CLASS_NAME);
         NativeException nativeException = new NativeException(runtime, nativeClass, cause);
         throw new RaiseException(cause, nativeException);
     }
-    
+
 }
