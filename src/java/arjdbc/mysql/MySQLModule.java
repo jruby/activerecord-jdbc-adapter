bc872dffc561ca99a7050377b1f620f5c925084d
kares <self@kares.org>
2013-08-01 22:16:47 +0200

    "native" quote_column_name for MySQL  (currently not used)

--- src/java/arjdbc/mysql/MySQLModule.java
+++ src/java/arjdbc/mysql/MySQLModule.java
@@ -26,6 +26,7 @@ package arjdbc.mysql;
 
 import static arjdbc.util.QuotingUtils.BYTES_0;
 import static arjdbc.util.QuotingUtils.BYTES_1;
+import static arjdbc.util.QuotingUtils.quoteCharWith;
 
 import org.jcodings.specific.UTF8Encoding;
 
@@ -116,6 +117,30 @@ public class MySQLModule {
             final ThreadContext context,
             final IRubyObject self) {
         return RubyString.newString(context.getRuntime(), BYTES_0);
+    }
+
+    @JRubyMethod(name = "quote_column_name", required = 1, frame = false)
+    public static IRubyObject quote_column_name(
+            final ThreadContext context,
+            final IRubyObject self,
+            final IRubyObject string) { // "`#{name.to_s.gsub('`', '``')}`"
+        RubyString origin = string.asString();
+        RubyString quoted = quoteCharWith(context, origin, '`', '`');
+        final ByteList bytes = quoted.getByteList();
+        if ( origin == quoted ) {
+            final RubyString result =
+                RubyString.newStringLight(context.getRuntime(), bytes.getRealSize() + 2, bytes.getEncoding());
+            final ByteList resultBytes = result.getByteList();
+            resultBytes.append( (byte) '`' );
+            resultBytes.append( bytes );
+            resultBytes.append( (byte) '`' );
+            return result;
+        }
+        else {
+            bytes.append( (byte) '`' );
+            bytes.prepend( (byte) '`' );
+            return quoted;
+        }
     }
 
 }
