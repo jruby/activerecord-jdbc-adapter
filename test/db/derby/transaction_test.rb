50fa0abb7942601f26dd0fad54abfbe347b3812c
kares <self@kares.org>
2013-08-02 21:31:44 +0200

    embedded db isolations are really primitive (lock the whole table)

--- test/db/derby/transaction_test.rb
+++ test/db/derby/transaction_test.rb
@@ -8,4 +8,41 @@ class DerbyTransactionTest < Test::Unit::TestCase
     assert_true ActiveRecord::Base.connection.supports_savepoints?
   end
 
+  # @override whole table gets locked!
+  def test_transaction_isolation_read_committed
+    unless ActiveRecord::Base.connection.supports_transaction_isolation?
+      omit("transaction isolation not supported")
+    end
+
+    Entry.transaction(:isolation => :read_committed) do
+      assert_equal 0, Entry.count
+
+      Entry2.transaction do
+        Entry2.create
+        #assert_equal 0, Entry.count
+      end
+    end
+    assert_equal 1, Entry.count
+  end if Test::Unit::TestCase.ar_version('4.0')
+
+  # @override whole table gets locked!
+  def test_transaction_isolation_repeatable_read
+    unless ActiveRecord::Base.connection.supports_transaction_isolation?
+      omit("transaction isolation not supported")
+    end
+
+    # NOTE need to emulate this in another thread !
+    #entry = Entry.create(:title => '1234')
+
+    Entry.transaction(:isolation => :repeatable_read) do
+      #entry.reload
+      #Entry2.find(entry.id).update_attributes(:title => '567')
+
+      #entry.reload
+      #assert_equal '1234', entry.title
+    end
+    #entry.reload
+    #assert_equal '567', entry.title
+  end if Test::Unit::TestCase.ar_version('4.0')
+
 end
--- test/db/sqlite3/transaction_test.rb
+++ test/db/sqlite3/transaction_test.rb
@@ -4,6 +4,7 @@ require 'transaction'
 class SQLite3TransactionTest < Test::Unit::TestCase
   include TransactionTestMethods
 
+  # @override
   def test_supports_transaction_isolation
     assert ActiveRecord::Base.connection.supports_transaction_isolation?
     # NOTE: adapter tell us it supports but JDBC meta-data API returns false ?!
@@ -13,6 +14,7 @@ class SQLite3TransactionTest < Test::Unit::TestCase
 
   # supports only TRANSACTION_SERIALIZABLE and TRANSACTION_READ_UNCOMMITTED
 
+  # @override
   def test_transaction_isolation_read_committed
     assert ! ActiveRecord::Base.connection.supports_transaction_isolation?(:read_committed)
 
@@ -21,11 +23,20 @@ class SQLite3TransactionTest < Test::Unit::TestCase
     end
   end if Test::Unit::TestCase.ar_version('4.0')
 
+  # @override
   def test_transaction_isolation_repeatable_read
     assert ! ActiveRecord::Base.connection.supports_transaction_isolation?(:repeatable_read)
 
     assert_raise ActiveRecord::TransactionIsolationError do
       super
+    end
+  end if Test::Unit::TestCase.ar_version('4.0')
+
+  def test_transaction_isolation_read_uncommitted
+    Entry.transaction(:isolation => :read_uncommitted) do
+      assert_equal 0, Entry.count
+      Entry.create # Entry2.create
+      assert_equal 1, Entry.count
     end
   end if Test::Unit::TestCase.ar_version('4.0')
 
