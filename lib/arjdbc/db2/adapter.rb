95f035f38cb83d47864f691867ff6a04c5cecc53
kares <self@kares.org>
2013-08-05 13:03:41 +0200

    refactored `SerializedAttributes` helper to be included onto AR::Base

--- lib/arjdbc/db2/adapter.rb
+++ lib/arjdbc/db2/adapter.rb
@@ -17,18 +17,7 @@ module ArJdbc
       return if @@_initialized; @@_initialized = true
 
       require 'arjdbc/util/serialized_attributes'
-      ActiveRecord::Base.class_eval do
-        def after_save_with_db2_lob
-          lob_columns = self.class.columns.select { |c| c.sql_type =~ /blob|clob/i }
-          lob_columns.each do |column|
-            value = ::ArJdbc::Util::SerializedAttributes.dump_column_value(self, column)
-            next if value.nil? # already set NULL
-
-            self.class.connection.update_lob_value(self, column, value)
-          end
-        end
-      end
-      ActiveRecord::Base.after_save :after_save_with_db2_lob
+      Util::SerializedAttributes.setup /blob|clob/i, 'after_save_with_db2_lob'
     end
 
     # @see ActiveRecord::ConnectionAdapters::JdbcAdapter#jdbc_connection_class
--- lib/arjdbc/util/serialized_attributes.rb
+++ lib/arjdbc/util/serialized_attributes.rb
@@ -1,6 +1,26 @@
 module ArJdbc
   module Util
+    # Gets included into `ActiveRecord::Base` to support sending LOB values
+    # in a separate update SQL statement for DB adapters that need this.
     module SerializedAttributes
+
+      # protected
+
+      def update_lob_columns
+        klass = self.class
+        return unless type = klass.lob_type # e.g. /blob/i
+        klass.columns.each do |column|
+          next if column.sql_type !~ type
+          next if ( value = dump_column_value(column) ).nil?
+          klass.connection.update_lob_value(self, column, value)
+        end
+      end
+
+      private
+
+      def dump_column_value(column)
+        SerializedAttributes.dump_column_value(self, column)
+      end
 
       def self.dump_column_value(record, column)
         value = record[ name = column.name.to_s ]
@@ -16,6 +36,41 @@ module ArJdbc
           end
         end
         value
+      end
+
+      def self.setup(lob_type = nil, after_save_alias = nil)
+        ActiveRecord::Base.send :include, self # include SerializedAttributes
+        ActiveRecord::Base.lob_type = lob_type unless lob_type.nil?
+        if after_save_alias
+          ActiveRecord::Base.class_eval do
+            alias_method after_save_alias, 'update_lob_columns'
+          end
+          ActiveRecord::Base.after_save after_save_alias
+        else
+          ActiveRecord::Base.after_save 'update_lob_columns'
+        end
+      end
+
+      def self.included(base)
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+
+        def lob_type
+          @lob_type ||= begin
+            if superclass.respond_to?(:lob_type)
+              superclass.lob_type
+            else
+              /blob|clob/i
+            end
+          end
+        end
+
+        def lob_type=(type)
+          @lob_type = type
+        end
+
       end
 
     end
