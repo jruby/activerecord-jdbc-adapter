853bfdeff735913b03a1e52bbde4c872a0796613
kares <self@kares.org>
2013-08-23 20:31:58 +0200

    do not use the deprecated SQL.gsub syntax with explain + binds test

--- test/explain_support_test_methods.rb
+++ test/explain_support_test_methods.rb
@@ -19,10 +19,15 @@ module ExplainSupportTestMethods
   def test_explain_with_binds
     create_explain_data
 
+    # "SELECT * FROM entries JOIN users on entries.user_id = users.id WHERE entries.rating > ?"
+    arel = Arel::SelectManager.new Entry.arel_engine
+    arel.project Arel.star
+    arel.from arel_table = Entry.arel_table
+    arel.join(User.arel_table).on(arel_table[:user_id].eq User.arel_table[:id])
+    arel.where arel_table[:rating].gt Arel::Nodes::BindParam.new('?')
+
     binds = [ [ Entry.columns.find { |col| col.name.to_s == 'rating' }, 0 ] ]
-    pp = ActiveRecord::Base.connection.explain(
-      "SELECT * FROM entries JOIN users on entries.user_id = users.id WHERE entries.rating > ?", binds
-    )
+    pp = ActiveRecord::Base.connection.explain(arel, binds)
     puts "\n"; puts pp if PRINT_EXPLAIN_OUTPUT
     assert_instance_of String, pp
   end
