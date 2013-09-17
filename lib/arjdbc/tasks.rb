bf2ede41728a100087b6af1970814eb3918cccb8
Rajan Agaskar <ragaskar@gmail.com>
2013-09-17 15:48:05 -0700

    Fix setting arjdbc.tasks.skip boolean example
    Had a typo (missing 'b').

--- lib/arjdbc/tasks.rb
+++ lib/arjdbc/tasks.rb
@@ -1,5 +1,5 @@
 if defined?(Rake.application) && Rake.application
-  skip = ENV["SKIP_AR_JDBC_RAKE_REDEFINES"] # jruby -J-Darjdc.tasks.skip=true -S rake ...
+  skip = ENV["SKIP_AR_JDBC_RAKE_REDEFINES"] # jruby -J-Darjdbc.tasks.skip=true -S rake ...
   if ! Java::JavaLang::Boolean.getBoolean('arjdbc.tasks.skip') || ! ( skip && skip != 'false' )
     databases_rake = File.expand_path('tasks/databases.rake', File.dirname(__FILE__))
     if Rake.application.lookup("db:create")
