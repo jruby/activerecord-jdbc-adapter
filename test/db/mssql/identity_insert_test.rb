79880ab72926aee7ea1571aeeeccc23d719e4d89
Micah Jaffe <me@micahjaffe.com>
2013-09-22 15:59:53 -0700

    Adding tests for double quoted identity delimiter

--- test/db/mssql/identity_insert_test.rb
+++ test/db/mssql/identity_insert_test.rb
@@ -7,12 +7,15 @@ class MSSQLIdentityInsertTest < Test::Unit::TestCase
   def test_enable_identity_insert_when_necessary
     Entry.connection.execute("INSERT INTO entries([id], [title]) VALUES (333, 'Title')")
     Entry.connection.execute("INSERT INTO entries([title], [id]) VALUES ('Title', 344)")
+    Entry.connection.execute(%Q(INSERT INTO entries("id", "title") VALUES (444, 'Title')))
+    Entry.connection.execute(%Q(INSERT INTO entries("title", "id") VALUES ('Title', 455)))
     Entry.connection.execute("INSERT INTO entries(id, title) VALUES (666, 'Title')")
     Entry.connection.execute("INSERT INTO entries(id, title) (SELECT id+123, title FROM entries)")
   end
 
   def test_dont_enable_identity_insert_when_unnecessary
     Entry.connection.execute("INSERT INTO entries([title]) VALUES ('[id]')")
+    Entry.connection.execute(%Q(INSERT INTO entries("title") VALUES ('"a memorable quote"')))
   end
 
 end
