eac53fd3d7b2d2a1aa1f56bfa64bebbe9a7eabec
kares <self@kares.org>
2013-09-23 12:17:28 +0200

    [postgres] handle :connect_timeout, :sslmode, :keepalives & :krbsrvname similar to how AR + PG does

--- lib/arjdbc/postgresql/connection_methods.rb
+++ lib/arjdbc/postgresql/connection_methods.rb
@@ -11,9 +11,25 @@ ArJdbc::ConnectionMethods.module_eval do
     database = config[:database] || config[:dbname]
 
     config[:url] ||= "jdbc:postgresql://#{host}:#{port}/#{database}"
-    config[:url] << config[:pg_params] if config[:pg_params]
+    config[:url] << config[:pg_params] if config[:pg_params] # should go away
 
     config[:username] ||= config[:user] || Java::JavaLang::System.get_property("user.name")
+    options = ( config[:options] ||= {} )
+    # PG :connect_timeout - maximum time to wait for connection to succeed
+    if connect_timeout = config[:connect_timeout]
+      options['socketTimeout'] = connect_timeout # options['loginTimeout'] as well?
+    end
+    sslmode = config.key?(:sslmode) ? config[:sslmode] : config[:requiressl]
+    unless sslmode.nil? # PG :sslmode - disable|allow|prefer|require
+      # JRuby/JVM needs to be started with :
+      #  -Djavax.net.ssl.trustStore=mystore -Djavax.net.ssl.trustStorePassword=...
+      # or a non-validating connection might be used (for testing) :
+      #  :sslfactory = 'org.postgresql.ssl.NonValidatingFactory'
+      options['ssl'] = 'true' if sslmode == true || sslmode.to_s == 'require'
+    end
+    options['tcpKeepAlive'] = config[:keepalives] if config.key?(:keepalives)
+    options['kerberosServerName'] = config[:krbsrvname] if config[:krbsrvname]
+
     config[:driver] ||= defined?(::Jdbc::Postgres.driver_name) ? ::Jdbc::Postgres.driver_name : 'org.postgresql.Driver'
     config[:adapter_spec] ||= ::ArJdbc::PostgreSQL
     config[:adapter_class] = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter unless config.key?(:adapter_class)
