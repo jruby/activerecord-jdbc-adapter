672d6063cecafa6fc1467b5b7a51dce9498aa4bd
kares <self@kares.org>
2013-10-11 18:41:12 +0200

    more test code + make sure Model.primary_key is set as a string (due 3.1)

--- test/db/postgresql/a_custom_primary_key_test.rb
+++ test/db/postgresql/a_custom_primary_key_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 require 'db/postgres'
 
-# NOTE: named to execute before: 
+# NOTE: named to execute before:
 # - PostgresConnectionTest
 # - PostgresDbCreateTest
 # - PostgresDbDropTest
 # since on 3.1 otherwise starts weirdly failing (when full suite is run) :
-# 
-#   ActiveRecord::JDBCError: org.postgresql.util.PSQLException: ERROR: 
+#
+#   ActiveRecord::JDBCError: org.postgresql.util.PSQLException: ERROR:
 #   null value in column "uhash" violates not-null constraint
-#     Detail: Failing row contains (null, http://url.to).: 
+#     Detail: Failing row contains (null, http://url.to).:
 #     INSERT INTO "some_urls" ("url") VALUES ('http://url.to') RETURNING "uhash"
 #
 class PostgresACustomPrimaryKeyTest < Test::Unit::TestCase
-  
+
   class CreateUrls < ActiveRecord::Migration
     def self.up
       create_table 'some_urls', :id => false do |t|
@@ -26,7 +26,7 @@ class PostgresACustomPrimaryKeyTest < Test::Unit::TestCase
       drop_table 'some_urls'
     end
   end
-  
+
   def setup
     CreateUrls.up
   end
@@ -36,15 +36,21 @@ class PostgresACustomPrimaryKeyTest < Test::Unit::TestCase
   end
 
   class SomeUrl < ActiveRecord::Base
-    self.primary_key = :uhash
+    self.primary_key = 'uhash' # :uhash won't work correctly on 3.1
   end
-  
+
   def test_create_url
+    url = SomeUrl.new
+    url.uhash = 'uhash1'
+    url.url = 'http://url.to'
+    url.save!
+    assert_equal 'uhash1', url.reload.uhash
+
     url = SomeUrl.create! do |instance|
-      instance.uhash = 'uhash'
+      instance.uhash = 'uhash2'
       instance.url = 'http://url.to'
     end
-    assert_equal 'uhash', url.reload.uhash
+    assert_equal 'uhash2', url.reload.uhash
   end
-  
+
 end
