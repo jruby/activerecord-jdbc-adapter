3ea62e3f743fccf037234becdfed238a057bc5e7
kares <self@kares.org>
2013-11-08 12:36:37 +0100

    [postgres] support some of libpg's ENV variables when connecting, namely: - PGHOST - PGPORT - PGUSER - PGDATABASE - PGPASSWORD - PGCONNECT_TIMEOUT

--- lib/arjdbc/postgresql/connection_methods.rb
+++ lib/arjdbc/postgresql/connection_methods.rb
@@ -6,21 +6,24 @@ ArJdbc::ConnectionMethods.module_eval do
     rescue LoadError # assuming driver.jar is on the class-path
     end
 
-    host = config[:host] ||= ( config[:hostaddr] || 'localhost' )
-    port = config[:port] ||= 5432
-    database = config[:database] || config[:dbname]
+    host = config[:host] ||= ( config[:hostaddr] || ENV['PGHOST'] || 'localhost' )
+    port = config[:port] ||= ( ENV['PGPORT'] || 5432 )
+    database = config[:database] || config[:dbname] || ENV['PGDATABASE']
 
     config[:url] ||= "jdbc:postgresql://#{host}:#{port}/#{database}"
     config[:url] << config[:pg_params] if config[:pg_params] # should go away
 
-    config[:username] ||= config[:user] || ENV_JAVA['user.name']
+    config[:username] ||= ( config[:user] || ENV['PGUSER'] || ENV_JAVA['user.name'] )
+    config[:password] ||= ENV['PGPASSWORD'] unless config.key?(:password)
     properties = ( config[:properties] ||= {} )
     # PG :connect_timeout - maximum time to wait for connection to succeed
-    if connect_timeout = config[:connect_timeout]
+    if connect_timeout = ( config[:connect_timeout] || ENV['PGCONNECT_TIMEOUT'] )
       properties['socketTimeout'] ||= connect_timeout
       # NOTE: maybe set options['loginTimeout'] as well?
     end
     sslmode = config.key?(:sslmode) ? config[:sslmode] : config[:requiressl]
+    # NOTE: makes not much sense since this needs some JVM options :
+    # sslmode = ENV['PGSSLMODE'] || ENV['PGREQUIRESSL'] if sslmode.nil?
     unless sslmode.nil? # PG :sslmode - disable|allow|prefer|require
       # JRuby/JVM needs to be started with :
       #  -Djavax.net.ssl.trustStore=mystore -Djavax.net.ssl.trustStorePassword=...
