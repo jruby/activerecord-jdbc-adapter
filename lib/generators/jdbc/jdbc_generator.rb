ce983281190464cd6358e48e5b9445c829a5b24f
kares <self@kares.org>
2013-11-11 12:51:14 +0100

    NOTE: that jdbc generator is not needed (will only show up on Rails >= 3.0)

--- lib/generators/jdbc/jdbc_generator.rb
+++ lib/generators/jdbc/jdbc_generator.rb
@@ -6,4 +6,12 @@ class JdbcGenerator < Rails::Generators::Base
   def create_jdbc_files
     directory '.', '.'
   end
+
+  def self.desc(description=nil)
+    return super if description
+    "Description:\n" <<
+    "  Creates stubs that ensure AR-JDBC is bootstrapped in your Rails application.\n" <<
+    "  NOTE: you should not need this if you're on Rails >= 3.0 (using Bundler) ..."
+  end
+
 end
