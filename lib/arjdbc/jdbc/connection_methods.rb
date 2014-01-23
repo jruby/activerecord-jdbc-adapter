0a2d68e95b5c614d527350e83d54beb3afe64343
kares <self@kares.org>
2014-01-23 13:31:33 +0100

    warn when using `adapter: jdbc` configuration while there's a standalone 1

--- lib/arjdbc/jdbc/connection_methods.rb
+++ lib/arjdbc/jdbc/connection_methods.rb
@@ -8,7 +8,12 @@ module ArJdbc
 
     def jdbc_connection(config)
       adapter_class = config[:adapter_class]
-      adapter_class ||= ::ActiveRecord::ConnectionAdapters::JdbcAdapter
+      adapter_class ||= begin
+        if config[:adapter] == 'jdbc' && adapter = supported_adapter(config)
+          warn "DEPRECATED: use 'adapter: #{adapter}' instead of the current 'adapter: jdbc' configuration"
+        end
+        ::ActiveRecord::ConnectionAdapters::JdbcAdapter
+      end
       adapter_class.new(nil, logger, config)
     end
 
@@ -24,6 +29,24 @@ module ArJdbc
 
     def jndi_config?(config)
       ::ActiveRecord::ConnectionAdapters::JdbcConnection.jndi_config?(config)
+    end
+
+    def supported_adapter(config)
+      return unless driver = config[:driver]
+      case driver
+      when 'com.ibm.db2.jcc.DB2Driver' then 'db2'
+      when /derby/ then 'derby' # 'org.apache.derby.jdbc.EmbeddedDriver'
+      when 'org.firebirdsql.jdbc.FBDriver' then 'firebird'
+      when 'org.h2.Driver' then 'h2'
+      when 'org.hsqldb.jdbcDriver' then 'hsqldb'
+      when 'net.sourceforge.jtds.jdbc.Driver' then 'mssql'
+      when 'com.microsoft.sqlserver.jdbc.SQLServerDriver' then 'sqlserver'
+      when /mysql/i then 'mysql' # 'com.mysql.jdbc.Driver'
+      when /oracle/ then 'oracle' # 'oracle.jdbc.driver.OracleDriver'
+      when /postgre/i then 'postgresql' # 'org.postgresql.Driver'
+      when /sqlite/i then 'sqlite3' # 'org.sqlite.JDBC'
+      else false
+      end
     end
 
   end
