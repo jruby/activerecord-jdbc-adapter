9d93f408298c5e8e44afecae601b9de01026149c
kares <self@kares.org>
2014-01-28 16:36:02 +0100

    only do the patched select_limited_ids if connection is Derby's

--- lib/arjdbc/derby/active_record_patch.rb
+++ lib/arjdbc/derby/active_record_patch.rb
@@ -1,9 +1,10 @@
-# Needed because Rails is broken wrt to quoting of some values. 
-# Most databases are nice about it, but not Derby. 
+# Needed because Rails is broken wrt to quoting of some values.
+# Most databases are nice about it, but not Derby.
 # The real issue is that you can't compare a CHAR value to a NUMBER column.
 ActiveRecord::Associations::ClassMethods.module_eval do
   private
   def select_limited_ids_list(options, join_dependency)
+    return super unless connection.is_a?(ArJdbc::Derby)
     connection.select_all(
       construct_finder_sql_for_association_limiting(options, join_dependency),
       "#{name} Load IDs For Limited Eager Loading"
