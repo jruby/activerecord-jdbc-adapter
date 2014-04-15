2bb0f49e161ba2bbf01ad937b5178493e4cd0fc0
kares <self@kares.org>
2014-04-15 12:24:10 +0200

    less garbage in firebird's arel

--- lib/arel/visitors/firebird.rb
+++ lib/arel/visitors/firebird.rb
@@ -5,18 +5,15 @@ module Arel
     class Firebird < Arel::Visitors::ToSql
 
       def visit_Arel_Nodes_SelectStatement o, a = nil
-        lim_off = [
-          ("FIRST #{do_visit o.limit.expr, a}" if o.limit),
-          ("SKIP #{do_visit o.offset.expr, a}" if o.offset)
-        ].compact.join(' ').strip
+        lim_off = ''
+        lim_off << "FIRST #{do_visit o.limit.expr, a} " if o.limit
+        lim_off << " SKIP #{do_visit o.offset.expr, a}" if o.offset
+        lim_off.strip!
 
-        sql = [
-         o.cores.map { |x| do_visit_select_core x, a }.join,
-         ("ORDER BY #{o.orders.map { |x| do_visit x, a }.join(', ')}" unless o.orders.empty?),
-        ].compact.join ' '
+        sql = o.cores.map { |x| do_visit_select_core x, a }.join
+        sql << " ORDER BY #{o.orders.map { |x| do_visit x, a }.join(', ')}" unless o.orders.empty?
 
-        sql.sub!(/\A(\s*SELECT\s)/i, '\&' + lim_off + ' ') unless lim_off.empty?
-
+        sql.sub!(/\A(\s*SELECT\s)/i, "\&#{lim_off} ") unless lim_off.empty?
         sql
       end
 
