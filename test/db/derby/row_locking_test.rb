282925e0b312cd47c4056aa834f5da8a79daf799
kares <self@kares.org>
2014-04-26 23:29:14 +0200

    row locking with derby will only pass as expected with serializable isolation

--- test/db/derby/row_locking_test.rb
+++ test/db/derby/row_locking_test.rb
@@ -3,4 +3,21 @@ require 'row_locking_test_methods'
 
 class DerbyRowLockingTest < Test::Unit::TestCase
   include RowLockingTestMethods
+
+  private
+
+  def thread_helper
+    # will only work as expected when isolation is increased to serial :
+    Entry.connection.raw_connection.transaction_isolation = :serializable
+
+    yield
+  rescue Exception => exc
+    # Output backtrace, since otherwise we won't see anything until the main thread joins this thread.
+    display_exception(exc)
+    raise
+  ensure
+    # This is needed.  Otherwise, the database connections aren't returned to the pool and things break.
+    ActiveRecord::Base.connection_handler.clear_active_connections!
+  end
+
 end
