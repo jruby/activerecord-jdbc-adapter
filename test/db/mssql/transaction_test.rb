e5f3cd8f8341f7f392c1d8bbe0ae0b45efe3841a
kares <self@kares.org>
2014-06-04 12:47:19 +0200

    skip transaction tests on ms-sql, for some reason they hang - INVESTIGATION NEEDED

--- test/db/mssql/transaction_test.rb
+++ test/db/mssql/transaction_test.rb
@@ -8,4 +8,18 @@ class MSSQLTransactionTest < Test::Unit::TestCase
     assert_true ActiveRecord::Base.connection.supports_savepoints?
   end
 
+  def test_transaction_isolation_read_uncommitted
+    super
+  end if ar_version('4.0')
+
+  def test_transaction_isolation_read_committed
+    return skip('(somehow) HANGS "dead-locks" - needs investigation')
+    super
+  end if ar_version('4.0')
+
+  def test_transaction_isolation_repeatable_read
+    return skip('(somehow) HANGS "dead-locks" - needs investigation')
+    super
+  end if ar_version('4.0')
+
 end
