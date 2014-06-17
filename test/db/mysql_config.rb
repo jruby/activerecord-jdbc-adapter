542e6ca2932369160ad42ef2f1d6ba153254f7d1
kares <self@kares.org>
2014-06-17 14:25:22 +0200

    allow running tests using `rake test_mysql DRIVER=MariaDB`

--- test/db/mysql_config.rb
+++ test/db/mysql_config.rb
@@ -9,3 +9,16 @@ MYSQL_CONFIG = {
 unless ( ps = ENV['PREPARED_STATEMENTS'] || ENV['PS'] ).nil?
   MYSQL_CONFIG[:prepared_statements] = ps
 end
+
+if driver = ENV['DRIVER']
+  if driver =~ /maria/i
+    if driver.index('.').nil?
+      driver = 'org.mariadb.jdbc.Driver'
+    end
+    jars = File.expand_path('../jars', File.dirname(__FILE__))
+    if jar = Dir.glob("#{jars}/mariadb*.jar").last
+      load jar
+    end
+  end
+  MYSQL_CONFIG[:driver] = driver if driver.index('.')
+end
