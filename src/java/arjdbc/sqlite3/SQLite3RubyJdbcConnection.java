e3f284d67a70288a5e31e32ac0e25d8c5c2ccb04
kares <self@kares.org>
2014-08-07 08:38:05 +0200

    simplified code - less waste using a ruby array instance directly

--- src/java/arjdbc/sqlite3/SQLite3RubyJdbcConnection.java
+++ src/java/arjdbc/sqlite3/SQLite3RubyJdbcConnection.java
@@ -197,13 +197,13 @@ public class SQLite3RubyJdbcConnection extends RubyJdbcConnection {
     protected RubyArray mapTables(final Ruby runtime, final DatabaseMetaData metaData,
             final String catalog, final String schemaPattern, final String tablePattern,
             final ResultSet tablesSet) throws SQLException {
-        final List<IRubyObject> tables = new ArrayList<IRubyObject>(32);
+        final RubyArray tables = runtime.newArray(24);
         while ( tablesSet.next() ) {
             String name = tablesSet.getString(TABLES_TABLE_NAME);
             name = name.toLowerCase(); // simply lower-case for SQLite3
-            tables.add( RubyString.newUnicodeString(runtime, name) );
+            tables.append( RubyString.newUnicodeString(runtime, name) );
         }
-        return runtime.newArray(tables);
+        return tables;
     }
 
     private static class SavepointStub implements Savepoint {
