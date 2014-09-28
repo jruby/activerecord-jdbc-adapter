a2d4e86c4e808bacd123deb6dab6ee32b763a22b
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-09-28 09:57:40 +0530

    Rails added null: false constraint on timestamps columns
    - Commit - https://github.com/sgrif/rails/commit/ea3ba34506c72d636096245016b5ef9cfe27c566
    - This right now gives deprecation warning that in Rails 5.0, null: true
      will be changed to null: false by default.
    - Updated our models to be null: false.

--- test/models/thing.rb
+++ test/models/thing.rb
@@ -2,7 +2,7 @@ class CreateThings < ActiveRecord::Migration
   def self.up
     create_table :things, :id => false do |t|
       t.string :name
-      t.timestamps
+      t.timestamps :null => false
     end
     add_index :things, :name, :unique => true
     add_index :things, :created_at, :where => 'name IS NOT NULL', :name => "thing_partial_index"
