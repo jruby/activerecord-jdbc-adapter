02053615cb12a310df6096e99419836e55f11513
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-10-25 23:03:55 +0530

    Use the generic type map for all PG type registrations
    Commit - https://github.com/rails/rails/commit/4826a4ab348e59c3ac1f6c31fd73f93eabb91a3b

--- lib/arjdbc/postgresql/oid_types.rb
+++ lib/arjdbc/postgresql/oid_types.rb
@@ -122,6 +122,99 @@ module ArJdbc
         end
       end
 
+      def initialize_type_map(m)
+        register_class_with_limit m, 'int2', OID::Integer
+        m.alias_type 'int4', 'int2'
+        m.alias_type 'int8', 'int2'
+        m.alias_type 'oid', 'int2'
+        m.register_type 'float4', OID::Float.new
+        m.alias_type 'float8', 'float4'
+        m.register_type 'text', Type::Text.new
+        register_class_with_limit m, 'varchar', Type::String
+        m.alias_type 'char', 'varchar'
+        m.alias_type 'name', 'varchar'
+        m.alias_type 'bpchar', 'varchar'
+        m.register_type 'bool', Type::Boolean.new
+        register_class_with_limit m, 'bit', OID::Bit
+        register_class_with_limit m, 'varbit', OID::BitVarying
+        m.alias_type 'timestamptz', 'timestamp'
+        m.register_type 'date', OID::Date.new
+        m.register_type 'time', OID::Time.new
+
+        m.register_type 'money', OID::Money.new
+        m.register_type 'bytea', OID::Bytea.new
+        m.register_type 'point', OID::Point.new
+        m.register_type 'hstore', OID::Hstore.new
+        m.register_type 'json', OID::Json.new
+        m.register_type 'jsonb', OID::Jsonb.new
+        m.register_type 'cidr', OID::Cidr.new
+        m.register_type 'inet', OID::Inet.new
+        m.register_type 'uuid', OID::Uuid.new
+        m.register_type 'xml', OID::Xml.new
+        m.register_type 'tsvector', OID::SpecializedString.new(:tsvector)
+        m.register_type 'macaddr', OID::SpecializedString.new(:macaddr)
+        m.register_type 'citext', OID::SpecializedString.new(:citext)
+        m.register_type 'ltree', OID::SpecializedString.new(:ltree)
+
+        # FIXME: why are we keeping these types as strings?
+        m.alias_type 'interval', 'varchar'
+        m.alias_type 'path', 'varchar'
+        m.alias_type 'line', 'varchar'
+        m.alias_type 'polygon', 'varchar'
+        m.alias_type 'circle', 'varchar'
+        m.alias_type 'lseg', 'varchar'
+        m.alias_type 'box', 'varchar'
+
+        m.register_type 'timestamp' do |_, _, sql_type|
+          precision = extract_precision(sql_type)
+          OID::DateTime.new(precision: precision)
+        end
+
+        m.register_type 'numeric' do |_, fmod, sql_type|
+          precision = extract_precision(sql_type)
+          scale = extract_scale(sql_type)
+
+          # The type for the numeric depends on the width of the field,
+          # so we'll do something special here.
+          #
+          # When dealing with decimal columns:
+          #
+          # places after decimal  = fmod - 4 & 0xffff
+          # places before decimal = (fmod - 4) >> 16 & 0xffff
+          if fmod && (fmod - 4 & 0xffff).zero?
+            # FIXME: Remove this class, and the second argument to
+            # lookups on PG
+            Type::DecimalWithoutScale.new(precision: precision)
+          else
+            OID::Decimal.new(precision: precision, scale: scale)
+          end
+        end
+
+        load_additional_types(m)
+      end if ActiveRecord::VERSION.to_s >= '4.2'
+
+      def load_additional_types(type_map, oids = nil)
+        if supports_ranges?
+          query = <<-SQL
+              SELECT t.oid, t.typname, t.typelem, t.typdelim, t.typinput, r.rngsubtype, t.typtype, t.typbasetype
+              FROM pg_type as t
+              LEFT JOIN pg_range as r ON oid = rngtypid
+            SQL
+        else
+          query = <<-SQL
+              SELECT t.oid, t.typname, t.typelem, t.typdelim, t.typinput, t.typtype, t.typbasetype
+              FROM pg_type as t
+            SQL
+        end
+
+        if oids
+          query += "WHERE t.oid::integer IN (%s)" % oids.join(", ")
+        end
+
+        initializer = OID::TypeMapInitializer.new(type_map)
+        records = execute(query, 'SCHEMA')
+        initializer.run(records)
+      end if ActiveRecord::VERSION.to_s >= '4.2'
     end
   end
 end
