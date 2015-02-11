057f3cfc0c947f0817eeb95cd0beef79aed09814
kares <self@kares.org>
2015-02-11 21:04:33 +0100

    enable auto-loading of registered (rake) tasks, instead of loading all up front

--- lib/arjdbc/tasks/database_tasks.rb
+++ lib/arjdbc/tasks/database_tasks.rb
@@ -9,6 +9,18 @@ module ArJdbc
 
       register_tasks(/mariadb/, ActiveRecord::Tasks::MySQLDatabaseTasks)
 
+      # patch-in lazy (auto) loading with AR registered tasks :
+      module ActiveRecord::Tasks::DatabaseTasks
+        def class_for_adapter(adapter)
+          unless key = @tasks.keys.detect { |pattern| adapter[pattern] }
+            raise DatabaseNotSupported, "Rake tasks not supported by '#{adapter}' adapter"
+          end
+          tasks = @tasks[key] || JdbcDatabaseTasks
+          tasks = ArJdbc::Tasks.const_get(tasks) unless tasks.is_a?(Module)
+          tasks
+        end
+      end
+
     else
 
       @@tasks = {}
@@ -17,33 +29,34 @@ module ArJdbc
         @@tasks[pattern] = task
       end
 
-      def self.tasks_instance(config)
-        adapter = config['adapter']
+      def self.tasks_instance(config); adapter = config['adapter']
         key = @@tasks.keys.detect { |pattern| adapter[pattern] }
-        ( @@tasks[key] || JdbcDatabaseTasks ).new(config)
+        tasks = @@tasks[key] || JdbcDatabaseTasks
+        tasks = const_get(tasks) unless tasks.is_a?(Module)
+        tasks.new(config)
       end
 
     end
 
-    require 'arjdbc/tasks/jdbc_database_tasks'
-    require 'arjdbc/tasks/db2_database_tasks'
-    require 'arjdbc/tasks/derby_database_tasks'
-    require 'arjdbc/tasks/h2_database_tasks'
-    require 'arjdbc/tasks/hsqldb_database_tasks'
-    require 'arjdbc/tasks/mssql_database_tasks'
-    require 'arjdbc/tasks/oracle_database_tasks'
+    autoload :JdbcDatabaseTasks, 'arjdbc/tasks/jdbc_database_tasks'
+    autoload :DB2DatabaseTasks, 'arjdbc/tasks/db2_database_tasks'
+    autoload :DerbyDatabaseTasks, 'arjdbc/tasks/derby_database_tasks'
+    autoload :H2DatabaseTasks, 'arjdbc/tasks/h2_database_tasks'
+    autoload :HSQLDBDatabaseTasks, 'arjdbc/tasks/hsqldb_database_tasks'
+    autoload :MSSQLDatabaseTasks, 'arjdbc/tasks/mssql_database_tasks'
+    autoload :OracleDatabaseTasks, 'arjdbc/tasks/oracle_database_tasks'
 
     # re-invent built-in (but deprecated on 4.0) tasks :
-    register_tasks(/sqlserver/, MSSQLDatabaseTasks)
-    register_tasks(/(oci|oracle)/, OracleDatabaseTasks)
-    register_tasks(/mssql/, MSSQLDatabaseTasks) # (built-in) alias
+    register_tasks(/sqlserver/, :MSSQLDatabaseTasks)
+    register_tasks(/(oci|oracle)/, :OracleDatabaseTasks)
+    register_tasks(/mssql/, :MSSQLDatabaseTasks) # (built-in) alias
     # tasks for custom (JDBC) adapters :
-    register_tasks(/db2/, DB2DatabaseTasks)
-    register_tasks(/derby/, DerbyDatabaseTasks)
-    register_tasks(/h2/, H2DatabaseTasks)
-    register_tasks(/hsqldb/, HSQLDBDatabaseTasks)
+    register_tasks(/db2/, :DB2DatabaseTasks)
+    register_tasks(/derby/, :DerbyDatabaseTasks)
+    register_tasks(/h2/, :H2DatabaseTasks)
+    register_tasks(/hsqldb/, :HSQLDBDatabaseTasks)
     # (default) generic JDBC task :
-    register_tasks(/^jdbc$/, JdbcDatabaseTasks)
+    register_tasks(/^jdbc$/, :JdbcDatabaseTasks)
 
     # NOTE: no need to register "built-in" adapters such as MySQL
     # - on 4.0 these are registered and will be instantiated
