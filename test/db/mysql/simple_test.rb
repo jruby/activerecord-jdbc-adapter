e0ff0766a37b1e0299ad524fcce1e30b6ac6662a
kares <self@kares.org>
2015-02-11 22:31:59 +0100

    last piece of (mysql) test to get `rake test_mariadb` green

--- test/db/mysql/simple_test.rb
+++ test/db/mysql/simple_test.rb
@@ -221,8 +221,9 @@ class MySQLSimpleTest < Test::Unit::TestCase
 
   test "config :host" do
     skip unless MYSQL_CONFIG[:database] # JDBC :url defined instead
+    adapter = MYSQL_CONFIG[:adapter] # mysql or mariadb
     begin
-      config = { :adapter => 'mysql', :port => 3306 }
+      config = { :adapter => adapter, :port => 3306 }
       config[:username] = MYSQL_CONFIG[:username]
       config[:password] = MYSQL_CONFIG[:password]
       config[:database] = MYSQL_CONFIG[:database]
