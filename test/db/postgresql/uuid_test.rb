a67d58a3bad075a54b7a76c73a5d398d248e2171
kares <self@kares.org>
2015-04-13 17:23:23 +0200

    add (failing) big_serial schema dump test from Rails 4.2

--- test/db/postgresql/uuid_test.rb
+++ test/db/postgresql/uuid_test.rb
@@ -55,3 +55,24 @@ class PostgresqlUUIDTest < Test::Unit::TestCase
   end
 
 end if Test::Unit::TestCase.ar_version('4.0')
+
+class PostgresqlLargeKeysTest < Test::Unit::TestCase # ActiveRecord::TestCase
+
+  def setup
+    connection.create_table('big_serials', :id => :bigserial) do |t|
+      t.string 'name'
+    end
+  end
+
+  def test_omg
+    schema = StringIO.new
+    ActiveRecord::SchemaDumper.dump(connection, schema)
+    assert_match "create_table \"big_serials\", id: :bigserial, force: :cascade",
+      schema.string
+  end
+
+  def teardown
+    connection.drop_table "big_serials"
+  end
+
+end if Test::Unit::TestCase.ar_version('4.2')
