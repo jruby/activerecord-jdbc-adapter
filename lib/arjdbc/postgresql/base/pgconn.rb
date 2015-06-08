0df01336e8e341197eb1a78082cac6a0309c8d18
Uwe Kubosch <uwe@kubosch.no>
2015-06-08 14:24:53 +0200

    Update pgconn.rb

--- lib/arjdbc/postgresql/base/pgconn.rb
+++ lib/arjdbc/postgresql/base/pgconn.rb
@@ -1,5 +1,8 @@
-class PGconn
-  def self.unescape_bytea(escaped)
-    String.from_java_bytes Java::OrgPostgresqlUtil::PGbytea.toBytes escaped.to_java_bytes
+require 'active_record/connection_adapters/postgresql/oid'
+module ActiveRecord::ConnectionAdapters::PostgreSQL::OID
+  class PGconn
+    def self.unescape_bytea(escaped)
+      String.from_java_bytes Java::OrgPostgresqlUtil::PGbytea.toBytes escaped.to_java_bytes
+    end
   end
 end
