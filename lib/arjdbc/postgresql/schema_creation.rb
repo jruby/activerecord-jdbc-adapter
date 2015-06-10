521bf91f8e395d518c58473161c64a4198760699
kares <self@kares.org>
2015-06-10 12:49:24 +0200

    [postgres] AR 4.2 compatibility with SchemaCreation e.g.  adding an array column with default: [] will now quote correctly

--- lib/arjdbc/postgresql/schema_creation.rb
+++ lib/arjdbc/postgresql/schema_creation.rb
@@ -1,6 +1,6 @@
 module ArJdbc
   module PostgreSQL
-    # @private copied from native adapter 4.0/4.1
+    # @private copied (and adjusted) from native adapter 4.0/4.1/4.2
     class SchemaCreation < ::ActiveRecord::ConnectionAdapters::AbstractAdapter::SchemaCreation
 
       private
@@ -9,7 +9,7 @@ module ArJdbc
         sql_type = type_to_sql(o.type.to_sym, o.limit, o.precision, o.scale)
         sql = "ADD COLUMN #{quote_column_name(o.name)} #{sql_type}"
         add_column_options!(sql, column_options(o))
-      end
+      end unless AR42_COMPAT
 
       def visit_ColumnDefinition(o)
         sql = super
@@ -18,7 +18,16 @@ module ArJdbc
           add_column_options!(sql, column_options(o))
         end
         sql
-      end
+      end unless AR42_COMPAT
+
+      def visit_ColumnDefinition(o)
+        sql = super
+        if o.primary_key? && o.type != :primary_key
+          sql << " PRIMARY KEY "
+          add_column_options!(sql, column_options(o))
+        end
+        sql
+      end if AR42_COMPAT
 
       def add_column_options!(sql, options)
         if options[:array] || options[:column].try(:array)
@@ -32,6 +41,15 @@ module ArJdbc
           super
         end
       end
+
+      def type_for_column(column)
+        if column.array
+          @conn.lookup_cast_type("#{column.sql_type}[]")
+        else
+          super
+        end
+      end if AR42_COMPAT
+
     end
 
     def schema_creation
