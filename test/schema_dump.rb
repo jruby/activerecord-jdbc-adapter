f2572f0cdfd092577cac0bef7408ffeedf774145
kares <self@kares.org>
2015-06-12 12:48:51 +0200

    adapt schema dump test on AR 4.2 ... *t.string \"id\", limit: 255, null: false* now

--- test/schema_dump.rb
+++ test/schema_dump.rb
@@ -137,7 +137,7 @@ module SchemaDumpTestMethods
     assert_not_nil(match, "string_ids table not found")
     assert_match %r((:id => false)|(id: false)), match[1], "no table id not preserved"
     if ar_version('4.2')
-      assert_match %r{t.string[[:space:]]+"id",[[:space:]]+null:[[:space:]]+false$}, match[2], "non-primary key id column not preserved"
+      assert_match %r{t.string[[:space:]]+"id",[[:space:]]+limit:[[:space:]]+255,[[:space:]]+null:[[:space:]]+false$}, match[2], "non-primary key id column not preserved"
     elsif ar_version('4.0')
       assert_match %r{t.string[[:space:]]+"id",[[:space:]]+null: false$}, match[2], "non-primary key id column not preserved"
     else
