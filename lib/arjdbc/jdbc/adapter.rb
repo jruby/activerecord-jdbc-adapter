1e36d460495042e52613fbb0e8c4f6ee410cd20c
Uwe Kubosch <uwe@kubosch.no>
2015-07-15 22:52:30 +0200

    Let RubyJdbcConnection set the cast_type

--- lib/arjdbc/jdbc/adapter.rb
+++ lib/arjdbc/jdbc/adapter.rb
@@ -305,21 +305,7 @@ module ActiveRecord
       end
 
       def columns(table_name, name = nil)
-        columns = @connection.columns(table_name.to_s)
-        # FIXME(uwe): RubyJdbcConnection instantiates the Column objects, but
-        # does not have access to adapter#lookup_cast_type so we have to add it
-        # here.
-        if ArJdbc::AR42
-          columns.map! do |col|
-            if col.cast_type == :cast_type
-              col.with_type(lookup_cast_type(col.sql_type))
-            else
-              col
-            end
-          end
-        end
-        # EMXIF
-        columns
+        @connection.columns(table_name.to_s)
       end
 
       # Starts a database transaction.
