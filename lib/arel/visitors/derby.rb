601b4b50bcff1bceb99bf548b68a231ab52e547e
Uwe Kubosch <uwe@kubosch.no>
2015-07-15 22:53:21 +0200

    Adapt visiting to AR42

--- lib/arel/visitors/derby.rb
+++ lib/arel/visitors/derby.rb
@@ -4,13 +4,32 @@ module Arel
   module Visitors
     class Derby < Arel::Visitors::ToSql
 
-      def visit_Arel_Nodes_SelectStatement o, a = nil
-        sql = o.cores.map { |x| do_visit(x, a) }.join
-        sql << " ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?
-        sql << " #{do_visit o.offset, a}" if o.offset
-        sql << " #{do_visit o.limit, a}" if o.limit
-        sql << " #{do_visit o.lock, a}" if o.lock
-        sql
+      def visit_Arel_Nodes_SelectStatement(o, a = nil)
+        if ArJdbc::AR42
+          o.cores.each { |x| do_visit(x, a) }
+          unless o.orders.empty?
+            a << ' ORDER BY '
+            do_visit(o.orders.first, a)
+            o.orders[1..-1].each do |x|
+              a << ', '
+              do_visit(x, a)
+            end
+          end
+          a << " #{do_visit o.offset, a}" if o.offset
+          a << " #{do_visit o.limit, a}" if o.limit
+          if o.lock
+            a << ' '
+            do_visit(o.lock, a)
+          end
+          a
+        else
+          sql = o.cores.map { |x| do_visit(x, a) }.join
+          sql << " ORDER BY #{o.orders.map { |x| do_visit x, a }.join(', ')}" unless o.orders.empty?
+          sql << " #{do_visit o.offset, a}" if o.offset
+          sql << " #{do_visit o.limit, a}" if o.limit
+          sql << " #{do_visit o.lock, a}" if o.lock
+          sql
+        end
       end
 
       def visit_Arel_Nodes_Limit o, a = nil
@@ -32,8 +51,14 @@ module Arel
       VALUES_DEFAULT = 'VALUES ( DEFAULT )' # NOTE: marker set by ArJdbc::Derby
 
       def visit_Arel_Nodes_InsertStatement o, a = nil
-        sql = "INSERT INTO "
-        sql << visit(o.relation, a)
+        if ArJdbc::AR42
+          sql = a
+          sql << "INSERT INTO "
+          visit(o.relation, a)
+        else
+          sql = "INSERT INTO "
+          sql << visit(o.relation, a)
+        end
 
         values = o.values
 
@@ -48,7 +73,13 @@ module Arel
           values = false
         end
 
-        sql << visit(values, a) if values
+        if values
+          if ArJdbc::AR42
+            visit(values, a)
+          else
+            sql << visit(values, a)
+          end
+        end
         sql
       end if Arel::VERSION >= '4.0' # AR 4.0 ... AREL 5.0 since AR >= 4.1
 
