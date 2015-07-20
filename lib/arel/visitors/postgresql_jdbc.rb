fe5c055fc58408a6139452c39959cd977aae4160
Uwe Kubosch <uwe@kubosch.no>
2015-07-20 14:42:15 +0200

    Added missing AR42 condition Maybe it should be a test for AREL version, not AR version?

--- lib/arel/visitors/postgresql_jdbc.rb
+++ lib/arel/visitors/postgresql_jdbc.rb
@@ -2,5 +2,5 @@ require 'arel/visitors/compat'
 
 class Arel::Visitors::PostgreSQL
   # AREL converts bind argument markers "?" to "$n" for PG, but JDBC wants "?".
-  remove_method :visit_Arel_Nodes_BindParam
+  remove_method :visit_Arel_Nodes_BindParam if ArJdbc::AR42
 end
