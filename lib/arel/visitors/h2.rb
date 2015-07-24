c6b7917916ea3274292d9e20dead3008c424b92f
Uwe Kubosch <uwe@kubosch.no>
2015-07-24 23:00:28 +0200

    H2 uses "-1" to indicate "no limit" while HSQLDB uses "0"

--- lib/arel/visitors/h2.rb
+++ lib/arel/visitors/h2.rb
@@ -8,6 +8,18 @@ module Arel
         o.limit ||= Arel::Nodes::Limit.new(-1) if o.offset
         super
       end if ArJdbc::AR42
+
+      def limit_offset sql, o
+        offset = o.offset || 0
+        offset = offset.expr unless (offset.nil? || offset == 0)
+        if limit = o.limit
+          "SELECT LIMIT #{offset} #{limit_for(limit)} #{sql[7..-1]}"
+        elsif offset > 0
+          "SELECT LIMIT #{offset} -1 #{sql[7..-1]}" # removes "SELECT "
+        else
+          sql
+        end
+      end unless ArJdbc::AR42
     end
   end
 end
