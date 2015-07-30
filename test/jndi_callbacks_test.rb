146eb808f30beb33b9a38e64ccd990054db55737
kares <self@kares.org>
2015-07-30 07:32:43 +0200

    align jndi callbacks test with AR 4.2 (active_connection? returns thread)

--- test/jndi_callbacks_test.rb
+++ test/jndi_callbacks_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 require 'db/jndi_config'
 
 class JndiConnectionPoolCallbacksTest < Test::Unit::TestCase
-  
+
   class Dummy < ActiveRecord::Base; end
-  
+
   setup do
     Dummy.establish_connection JNDI_CONFIG.dup
   end
-  
+
   teardown do
     Dummy.remove_connection
   end
-  
+
   test 'calls hooks on checkout and checkin' do
     connection = Dummy.connection_pool.checkout
     assert_true connection.active?
@@ -20,11 +20,12 @@ class JndiConnectionPoolCallbacksTest < Test::Unit::TestCase
     # connection = Dummy.connection
     Dummy.connection_pool.checkin connection
     assert_false connection.active?
-    
+
     pool = Dummy.connection_pool
     assert_false pool.active_connection? if pool.respond_to?(:active_connection?)
     assert_true pool.connection.active? # checks out
-    assert_true pool.active_connection? if pool.respond_to?(:active_connection?)
+    # assert_true works up until 4.2 where Thread instance is returned instead :
+    assert pool.active_connection? if pool.respond_to?(:active_connection?)
     assert_true connection.active?
     Dummy.connection_pool.disconnect!
     assert_false connection.active?
