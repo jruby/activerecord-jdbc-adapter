743a57bfa47cbac8a8d3bbbf82662f299a18eae4
kares <self@kares.org>
2015-09-07 16:01:48 +0200

    [firebird] boolean parameter with prepared-statements should be set as CHAR

--- src/java/arjdbc/firebird/FirebirdRubyJdbcConnection.java
+++ src/java/arjdbc/firebird/FirebirdRubyJdbcConnection.java
@@ -61,6 +61,33 @@ public class FirebirdRubyJdbcConnection extends RubyJdbcConnection {
         }
     };
 
+    @Override // booleans are emulated can not setNull(index, Types.BOOLEAN)
+    protected void setBooleanParameter(final ThreadContext context,
+        final Connection connection, final PreparedStatement statement,
+        final int index, final Object value,
+        final IRubyObject column, final int type) throws SQLException {
+        if ( value instanceof IRubyObject ) {
+            setBooleanParameter(context, connection, statement, index, (IRubyObject) value, column, type);
+        }
+        else {
+            if ( value == null ) statement.setNull(index, Types.CHAR);
+            else {
+                statement.setBoolean(index, ((Boolean) value).booleanValue());
+            }
+        }
+    }
+
+    @Override // booleans are emulated can not setNull(index, Types.BOOLEAN)
+    protected void setBooleanParameter(final ThreadContext context,
+        final Connection connection, final PreparedStatement statement,
+        final int index, final IRubyObject value,
+        final IRubyObject column, final int type) throws SQLException {
+        if ( value.isNil() ) statement.setNull(index, Types.CHAR);
+        else {
+            statement.setBoolean(index, value.isTrue());
+        }
+    }
+
     protected IRubyObject jdbcToRuby(
         final ThreadContext context, final Ruby runtime,
         final int column, final int type, final ResultSet resultSet)
