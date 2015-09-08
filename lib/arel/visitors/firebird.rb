4de792990cda5f61f73c63363f8429aaf8ba416d
kares <self@kares.org>
2015-09-08 09:10:55 +0200

    [firebird] tune arel visitor (limit/offset handling) to work on AR 4.2

--- lib/arel/visitors/firebird.rb
+++ lib/arel/visitors/firebird.rb
@@ -4,19 +4,69 @@ module Arel
   module Visitors
     class Firebird < Arel::Visitors::ToSql
 
-      def visit_Arel_Nodes_SelectStatement o, a = nil
-        lim_off = ''
-        lim_off << "FIRST #{do_visit o.limit.expr, a} " if o.limit
-        lim_off << " SKIP #{do_visit o.offset.expr, a}" if o.offset
-        lim_off.strip!
+      if ArJdbc::AR42
+        def visit_Arel_Nodes_SelectStatement(o, a)
+          a = o.cores.inject(a) { |c, x| visit_Arel_Nodes_SelectCore(x, c) }
 
-        sql = o.cores.map { |x| do_visit_select_core x, a }.join
-        sql << " ORDER BY #{o.orders.map { |x| do_visit x, a }.join(', ')}" unless o.orders.empty?
+          if ( limit = o.limit ) || ( offset = o.offset)
+            select = a.parts[0]
 
-        sql.sub!(/\A(\s*SELECT\s)/i, '\&' + lim_off + ' ') unless lim_off.empty?
-        sql
+            sql = Arel::Collectors::SQLString.new
+            visit(limit, sql) if limit
+            visit(offset, sql) if offset
+
+            a.parts[0] = "#{select} #{sql.value}"
+          end
+
+          unless o.orders.empty?
+            a << ' ORDER BY '
+            last = o.orders.length - 1
+            o.orders.each_with_index do |x, i|
+              visit(x, a);  a << ', ' unless last == i
+            end
+          end
+
+          a
+        end
+
+        def visit_Arel_Nodes_Limit(o, a)
+          a << "FIRST #{limit_for(o)}"
+        end
+
+        def visit_Arel_Nodes_Offset(o, a)
+          a << 'SKIP '; visit(o.value, a)
+        end
+
+      else
+
+        # @private
+        SELECT_RE = /\A(\s*SELECT\s)/i
+
+        def visit_Arel_Nodes_SelectStatement o, a = nil
+          lim_off = ''
+          lim_off << "FIRST #{do_visit o.limit.expr, a} " if o.limit
+          lim_off << " SKIP #{do_visit o.offset.expr, a}" if o.offset
+          lim_off.strip!
+
+          sql = o.cores.map { |x| do_visit_select_core x, a }.join
+          sql.sub!(SELECT_RE, "\\&#{lim_off} ") unless lim_off.empty?
+
+          unless o.orders.empty?
+            sql << ' ORDER BY '
+            last = o.orders.length - 1
+            o.orders.each_with_index do |x, i|
+              sql << do_visit(x, a);  sql << ', ' unless last == i
+            end
+          end
+
+          sql
+        end
       end
 
     end
   end
 end
+
+Arel::Collectors::Bind.class_eval do
+  attr_reader :parts
+end if defined? Arel::Collectors::Bind
