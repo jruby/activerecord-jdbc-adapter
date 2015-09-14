1153bf1aeeb1f38c1c305c9b2898ae8bb55948d1
kares <self@kares.org>
2015-09-14 14:47:26 +0200

    warn on mimer and informix adapters as unsupported on 4.2

--- lib/arjdbc/informix.rb
+++ lib/arjdbc/informix.rb
@@ -2,3 +2,4 @@ require 'arjdbc'
 ArJdbc.load_java_part :Informix
 require 'arjdbc/informix/adapter'
 require 'arjdbc/informix/connection_methods'
+ArJdbc.warn_unsupported_adapter 'informix', [4, 2] # warns on AR >= 4.2
--- lib/arjdbc/mimer.rb
+++ lib/arjdbc/mimer.rb
@@ -1,2 +1,3 @@
 require 'arjdbc'
 require 'arjdbc/mimer/adapter'
+ArJdbc.warn_unsupported_adapter 'mimer', [4, 2] # warns on AR >= 4.2
