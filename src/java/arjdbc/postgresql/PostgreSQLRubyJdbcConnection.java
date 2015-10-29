5b5bf0be7fa75487467530b7392c5bfdd9b57503
kares <self@kares.org>
2015-10-29 15:23:54 +0100

    [postgres] minor semi-align `begin` impl to how AR core does (isolation set first)

--- src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
+++ src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
@@ -1,5 +1,5 @@
 /***** BEGIN LICENSE BLOCK *****
- * Copyright (c) 2012-2014 Karol Bucek <self@kares.org>
+ * Copyright (c) 2012-2015 Karol Bucek <self@kares.org>
  * Copyright (c) 2006-2010 Nick Sieger <nick@nicksieger.com>
  * Copyright (c) 2006-2007 Ola Bini <ola.bini@gmail.com>
  * Copyright (c) 2008-2009 Thomas E Enebo <enebo@acm.org>
@@ -105,6 +105,16 @@ public class PostgreSQLRubyJdbcConnection extends arjdbc.jdbc.RubyJdbcConnection
         }
 
         return driverWrapper;
+    }
+
+    protected final IRubyObject beginTransaction(final ThreadContext context, final Connection connection,
+        final IRubyObject isolation) throws SQLException {
+        // NOTE: only reversed order - just to ~ match how Rails does it :
+        /* if ( connection.getAutoCommit() ) */ connection.setAutoCommit(false);
+        if ( isolation != null ) {
+            setTransactionIsolation(context, connection, isolation);
+        }
+        return context.nil;
     }
 
     final DriverImplementation driverImplementation = new PGDriverImplementation(); // currently no other supported
