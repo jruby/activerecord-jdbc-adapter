b217886e166612284ee8a1c09324098f29fccd19
kares <self@kares.org>
2015-11-03 09:16:12 +0100

    align updated base schema dump tests for adapters on 4.2

--- test/db/h2/schema_dump_test.rb
+++ test/db/h2/schema_dump_test.rb
@@ -5,7 +5,7 @@ class H2SchemaDumpTest < Test::Unit::TestCase
   include SchemaDumpTestMethods
 
   DbTypeMigration.big_decimal_precision = 56 # whatever - arbitrary
-  
+
 #  # #override
 #  def test_schema_dump_keeps_id_column_when_id_is_false_and_id_column_added
 #    output = standard_dump
@@ -25,5 +25,20 @@ class H2SchemaDumpTest < Test::Unit::TestCase
       assert_match %r{t.decimal\s+"sample_small_decimal",\s+:precision => 3,\s+:scale => 2}, output
     end
   end
-  
+
+  # @override
+  def test_schema_dump_keeps_id_column_when_id_is_false_and_id_column_added
+    output = standard_dump
+    match = output.match(%r{create_table "string_ids"(.*)do.*\n(.*)\n})
+    assert_not_nil(match, "string_ids table not found")
+    assert_match %r((:id => false)|(id: false)), match[1], "no table id not preserved"
+    if ar_version('4.2')
+      assert_match %r{t.string[[:space:]]+"id",[[:space:]]+limit:[[:space:]]+255,[[:space:]]+null:[[:space:]]+false$}, match[2], "non-primary key id column not preserved"
+    elsif ar_version('4.0')
+      assert_match %r{t.string[[:space:]]+"id",[[:space:]]+null: false$}, match[2], "non-primary key id column not preserved"
+    else
+      assert_match %r{t.string[[:space:]]+"id",[[:space:]]+:null => false$}, match[2], "non-primary key id column not preserved"
+    end
+  end
+
 end
