a6297fadb41621e1ae5c324621782bdcd0dd6814
mattclar <mattclar@gmail.com>
2016-08-25 22:53:52 +1000

    replace require for array parser

--- lib/arjdbc/postgresql/oid/array.rb
+++ lib/arjdbc/postgresql/oid/array.rb
@@ -14,6 +14,7 @@ module ActiveRecord
             require 'pg_array_parser'
             include PgArrayParser
           rescue LoadError
+            require 'arjdbc/postgresql/base/array_parser.rb'
             include PostgreSQL::ArrayParser
           end
 
