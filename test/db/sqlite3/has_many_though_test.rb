2e6dc738cc56aa291193b2369669ecdc5ddc3f32
Thomas E. Enebo <tom.enebo@gmail.com>
2016-11-14 10:52:12 -0600

    AR Version has no to_s or access to @version.

--- test/db/sqlite3/has_many_though_test.rb
+++ test/db/sqlite3/has_many_though_test.rb
@@ -3,9 +3,4 @@ require 'has_many_through'
 
 class SQLite3HasManyThroughTest < Test::Unit::TestCase
   include HasManyThroughMethods
-
-  def self.startup
-    puts "Using SQLite: #{ActiveRecord::Base.connection.send(:sqlite_version)}"
-  end
-
 end
