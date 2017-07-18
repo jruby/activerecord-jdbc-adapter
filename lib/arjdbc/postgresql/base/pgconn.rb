34470c7342cfc8f53a8793e11d9c2b70f4ac2b7a
Rob Widmer <robert.widmer@finalsite.com>
2017-07-18 08:11:47 -0400

    Updated patch for PGConn to patch PG::Connection since 5.0.4 updated references to it

--- lib/arjdbc/postgresql/base/pgconn.rb
+++ lib/arjdbc/postgresql/base/pgconn.rb
@@ -1,5 +1,5 @@
-module ActiveRecord::ConnectionAdapters::PostgreSQL::OID
-  class PGconn # emulate PGconn#unescape_bytea due #652
+module PG
+  class Connection # emulate PGconn#unescape_bytea due #652
     # NOTE: on pg gem ... PGconn = (class) PG::Connection
     def self.unescape_bytea(escaped)
       ArJdbc::PostgreSQL.unescape_bytea(escaped)
