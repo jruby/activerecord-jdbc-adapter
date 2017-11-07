48d4ffc64b480ba013ba9e7ab6cbc335e734603c
Thomas E. Enebo <tom.enebo@gmail.com>
2017-11-07 11:00:16 -0600

    Update to newer JDBC adapter for mysql (5.1.33->5.1.44).

--- jdbc-mysql/lib/jdbc/mysql.rb
+++ jdbc-mysql/lib/jdbc/mysql.rb
@@ -5,7 +5,7 @@ module Jdbc
   module MySQL
 
     def self.driver_jar
-      "mysql-connector-java-#{DRIVER_VERSION}-bin.jar"
+      "mysql-connector-java-#{DRIVER_VERSION}.jar"
     end
 
     def self.load_driver(method = :load)
