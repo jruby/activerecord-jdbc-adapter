a5aa43cc3a42b4373cff3d29647c3adaf2d94df5
kares <self@kares.org>
2017-11-27 10:50:09 +0100

    [test] adjust Postgre's quoting tests - bit different but not much to care

--- test/db/postgresql/quoting_test.rb
+++ test/db/postgresql/quoting_test.rb
@@ -5,43 +5,25 @@ class PostgreSQLQuotingTest < Test::Unit::TestCase
 
   Column = ActiveRecord::ConnectionAdapters::Column
 
-
-  def test_type_cast_cidr
-    if ar_version('4.2')
-      # TODO port test
-    else
-      ip = IPAddr.new('255.0.0.0/8')
-      c = Column.new(nil, ip, 'cidr')
-      assert_equal ip, connection.type_cast(ip, c)
-    end
-  end if ar_version('4.0')
-
-  def test_type_cast_inet
-    if ar_version('4.2')
-      # TODO port test
-    else
-      ip = IPAddr.new('255.1.0.0/8')
-      c = Column.new(nil, ip, 'inet')
-      assert_equal ip, connection.type_cast(ip, c)
-    end
-  end if ar_version('4.0')
-
-  def test_quote_cast_numeric
+  def test_quote_fixnum
     fixnum = 666
-    if ar_version('4.2')
-      # TODO port test
-    else
-      c = Column.new(nil, nil, 'varchar')
-      assert_equal "'666'", connection.quote(fixnum, c)
-      c = Column.new(nil, nil, 'text')
-      assert_equal "'666'", connection.quote(fixnum, c)
-    end
+    assert_equal '666', connection.quote(fixnum)
   end
 
   def test_quote_time_usec
     time = Time.at(0) + (0.000001).seconds
-    assert_equal "'1970-01-01 00:00:00.000001'", connection.quote(time)
-    assert_equal "'1970-01-01 00:00:00.000001'", connection.quote(time.to_datetime)
+    if defined? JRUBY_VERSION
+      assert_equal "'1970-01-01 00:00:00.000001'", connection.quote(time)
+      assert_equal "'1970-01-01 00:00:00.000001'", connection.quote(time.to_datetime)
+    else # Rails-way opinionated as usual :
+      assert_equal "'1970-01-01 00:00:00'", connection.quote(time)
+      assert_equal "'1970-01-01 00:00:00'", connection.quote(time.to_datetime)
+    end
+
+    time += 0.001000.seconds
+    assert_equal "'1970-01-01 00:00:00.001001'", connection.quote(time)
+    time = Time.at(0) + (0.001).seconds
+    assert_equal "'1970-01-01 00:00:00.001000'", connection.quote(time.to_datetime)
   end
 
 end
