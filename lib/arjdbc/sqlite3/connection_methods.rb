33fdd654a59626d2f0172e857cb7dc0ee9357f59
kares <self@kares.org>
2017-12-03 13:55:18 +0100

    [sqlite3] redo NoDatabaseError handling to be more compatible (ugly)

--- lib/arjdbc/sqlite3/connection_methods.rb
+++ lib/arjdbc/sqlite3/connection_methods.rb
@@ -13,7 +13,16 @@ ArJdbc::ConnectionMethods.module_eval do
     end
     config[:driver] ||= 'org.sqlite.JDBC'
 
-    parse_sqlite3_config!(config)
+    begin
+      parse_sqlite3_config!(config)
+    rescue Errno::ENOENT => error
+      if error.message.include?('No such file or directory')
+        raise ActiveRecord::NoDatabaseError
+      else
+        raise
+      end
+    end
+
     database = config[:database] # NOTE: "jdbc:sqlite::memory:" syntax is supported
     config[:url] ||= "jdbc:sqlite:#{database == ':memory:' ? '' : database}"
     config[:connection_alive_sql] ||= 'SELECT 1'
@@ -27,22 +36,22 @@ ArJdbc::ConnectionMethods.module_eval do
     options['busy_timeout'] ||= timeout unless timeout.nil?
 
     jdbc_connection(config)
-  rescue ActiveRecord::JDBCError => error
-    if error.message =~ /path to .*? does not exist/ # path to '/foo/xxx/bar.db': '/foo/xxx' does not exist
-      raise ActiveRecord::NoDatabaseError
-    else
-      raise
-    end
+  # rescue ActiveRecord::JDBCError => error
+  #   if error.message =~ /path to .*? does not exist/ # path to '/foo/xxx/bar.db': '/foo/xxx' does not exist
+  #     raise ActiveRecord::NoDatabaseError
+  #   else
+  #     raise
+  #   end
   end
   alias_method :jdbcsqlite3_connection, :sqlite3_connection
 
   private
 
   def parse_sqlite3_config!(config)
-    database = ( config[:database] ||= config[:dbfile] ) # allow Rails relative path :
-    if database != ':memory:' && defined?(Rails.root)
-      config[:database] = File.expand_path(database, Rails.root.to_s)
-      dirname = File.dirname(config[:database])
+    database = ( config[:database] ||= config[:dbfile] )
+    if ':memory:' != database
+      config[:database] = File.expand_path(database, Rails.root) if defined?(Rails.root)
+      dirname = File.dirname(database)
       Dir.mkdir(dirname) unless File.directory?(dirname)
     end
   end
