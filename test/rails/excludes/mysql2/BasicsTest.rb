95467d61eeafb24a157bd58b44b44083035f31b1
kares <self@kares.org>
2018-03-28 16:07:25 +0200

    [test] reformat MySQL Rails exclude to be less confusing

--- test/rails/excludes/mysql2/BasicsTest.rb
+++ test/rails/excludes/mysql2/BasicsTest.rb
@@ -1,6 +1,3 @@
-[ # NOTE: these are copied to AR-JDBC's suite with proper (JVM) TZ adjustment
-  :test_preserving_time_objects_with_utc_time_conversion_to_default_timezone_local,
-  :test_preserving_time_objects_with_time_with_zone_conversion_to_default_timezone_local
-].each do |name|
-  exclude name, 'assuming ENV[TZ] change reflects system (JVM) TimeZone default change'
-end
+# NOTE: these are copied to AR-JDBC's suite with proper (JVM) TZ adjustment
+exclude :test_preserving_time_objects_with_utc_time_conversion_to_default_timezone_local, 'assuming ENV[TZ] change reflects system (JVM) TimeZone default change'
+exclude :test_preserving_time_objects_with_time_with_zone_conversion_to_default_timezone_local, 'assuming ENV[TZ] change reflects system (JVM) TimeZone default change'
