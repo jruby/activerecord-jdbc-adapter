8521b4c877f32d0f4918ec303a3a3537e88759cb
Rob Widmer <robert.widmer@finalsite.com>
2018-04-06 10:36:33 -0400

    [postgres] Ignore rails test for correct sql name logging that fails because of a different implementation

--- test/rails/excludes/postgresql/ActiveRecord/PostgresqlConnectionTest.rb
+++ test/rails/excludes/postgresql/ActiveRecord/PostgresqlConnectionTest.rb
@@ -1,1 +1,2 @@
 exclude :test_connection_options, 'Connection options not supported'
+exclude :test_table_alias_length_logs_name, 'We reuse max identifer length for this so no query is made to be logged for the rails test'
