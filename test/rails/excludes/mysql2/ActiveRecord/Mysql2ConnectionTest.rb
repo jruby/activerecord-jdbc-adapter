92624d8265afc334354c09ca770d65478d22a1a6
Thomas E. Enebo <tom.enebo@gmail.com>
2018-04-17 16:49:59 -0500

    I picked wrong namespace for that exclude

--- test/rails/excludes/mysql2/ActiveRecord/Mysql2ConnectionTest.rb
+++
@@ -1,1 +0,0 @@
-exclude :test_mysql_connection_collation_is_configured, "Issue #884"
--- test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
+++ test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
@@ -1,3 +1,4 @@
 exclude :test_passing_arbitary_flags_to_adapter, 'no support for arbitrary flags such as Mysql2::Client::COMPRESS'
 exclude :test_passing_flags_by_array_to_adapter, 'no support for passing flags: ... not supported by JDBC adapter'
 exclude :test_quote_after_disconnect, 'AR-JDBC does not rely on driver to do quoting - thus wont raise an error'
+exclude :test_mysql_connection_collation_is_configured, "Issue #884"
