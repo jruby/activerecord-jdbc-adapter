08bfc74ec5e8fa79b27ddb95065d5d47eb950bf5
kares <self@kares.org>
2018-09-13 10:00:09 +0200

    [postgres] be 'very' permissive in version number parsing
    we've seen some non-standard bits in the past, thus allow arbitrary
    'product' naming. after initial 'clean' PR tuning needed (GH-952).
    
    might be too much but we certainly do not want to be revisiting this due
    various distribution packaging and custom naming conventions.
    
    resolves GH-952

--- test/db/postgresql/version_test.rb
+++ test/db/postgresql/version_test.rb
@@ -4,32 +4,55 @@ require 'db/postgresql/test_helper'
 
 class VersionTest < Test::Unit::TestCase
   def test_pg_9_version
-    assert_equal connection_stub('9.6.8'), 90608
+    assert_equal 90608, connection_stub('9.6.8')
   end
 
   def test_pg_10_version
-    assert_equal connection_stub('10.4'), 100400
+    assert_equal 100400, connection_stub('10.4')
   end
 
   def test_pg_4part_version
-    assert_equal connection_stub('9.6.8.1'), 90608
+    assert_equal 90608, connection_stub('9.6.8.1')
+  end
+
+  def test_pg_path_after_4_part_version
+    assert_equal 90608, connection_stub('9.6.8.1.9')
+    assert_equal 90608, connection_stub('9.6.8.1.RC1')
+    assert_equal 90608, connection_stub('9.6.8-RC1')
   end
 
   def test_pg_failed_version
-    assert_equal connection_stub('9.6.8.1.9'), 0
+    assert_equal 0, connection_stub('FOO')
+  end
+
+  def test_pg_non_standard_prefix_version
+    assert_equal 100000, connection_stub('Product 10')
+    assert_equal 90400, connection_stub('(Custom Build) 9.4')
+    assert_equal 100000, connection_stub('PG 10.0.0', true)
+  end
+
+  def test_pg_version_with_os_version
+    assert_equal 100400, connection_stub('10.4 (Ubuntu 10.4-2.pgdg16.04+1)')
+  end
+
+  def test_custom_version
+    str = '8.0.2 on i686-pc-linux-gnu, compiled by GCC gcc (GCC) 3.4.2 20041017 (Red Hat 3.4.2-6.fc3), Redshift 1.0.647'
+    assert_equal 80002, connection_stub(str)
   end
 
   def test_pg_single_version
-    assert_equal connection_stub('9'), 90000
+    assert_equal 90000, connection_stub('9')
+    assert_equal 100000, connection_stub('10 RC1')
+    assert_equal 100000, connection_stub('10 RC1 10.1')
   end
 
   private
 
-  def connection_stub(version_string)
+  def connection_stub(version, full_version = false)
     connection = mock('connection')
-    connection.expects(:jndi?)
-    connection.expects(:configure_connection)
-    connection.stubs(:database_product).returns("PostgreSQL #{version_string}")
+    connection.stubs(:jndi?)
+    connection.stubs(:configure_connection)
+    connection.expects(:database_product).returns full_version ? version.to_s : "PostgreSQL #{version}"
     ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.any_instance.stubs(:initialize_type_map)
     pg_connection = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new(connection, nil, {})
     pg_connection.postgresql_version
