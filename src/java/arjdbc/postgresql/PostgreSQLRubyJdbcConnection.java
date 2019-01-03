jruby/activerecord-jdbc-adapter

commit 8cb5a2ac8c970aad0384b0bd21a1a2b164d46d0c

[postgres] use timestamp string helper for all timestamps
pgjdbc always converts the Timestamp value to a string. Instead of
  RubyTime -> Timestamp -> Calendar -> StringBuilder -> String
use the new helpers for the more direct
  RubyTime -> StringBuilder -> String

src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
@@ -328,47 +328,9 @@ public class PostgreSQLRubyJdbcConnection extends arjdbc.jdbc.RubyJdbcConnection
         final Connection connection, final PreparedStatement statement,
         final int index, IRubyObject value,
         final IRubyObject attribute, final int type) throws SQLException {
-
-        if ( value instanceof RubyFloat ) {
-            final double doubleValue = ( (RubyFloat) value ).getValue();
-            if ( Double.isInfinite(doubleValue) ) {
-                setTimestampInfinity(statement, index, doubleValue);
-                return;
-            }
-        }
-
-        RubyTime timeValue = toTime(context, value);
-
-        final Timestamp timestamp;
-
-        if (timeValue.getDateTime().getYear() > 0) {
-            timeValue = timeInDefaultTimeZone(context, timeValue);
-            DateTime dateTime = timeValue.getDateTime();
-            timestamp = new Timestamp(dateTime.getMillis());
-
-            if (timeValue.getNSec() > 0) timestamp.setNanos((int) (timestamp.getNanos() + timeValue.getNSec()));
-
-            statement.setTimestamp(index, timestamp, getCalendar(dateTime.getZone()));
-        }
-        else {
-            setTimestampBC(statement, index, timeValue);
-        }
-    }
-
-    private static void setTimestampBC(final PreparedStatement statement,
-                                       final int index, final RubyTime timeValue) throws SQLException {
-        DateTime dateTime = timeValue.getDateTime();
-        @SuppressWarnings("deprecated")
-        Timestamp timestamp = new Timestamp(dateTime.getYear() - 1900,
-                dateTime.getMonthOfYear() - 1,
-                dateTime.getDayOfMonth(),
-                dateTime.getHourOfDay(),
-                dateTime.getMinuteOfHour(),
-                dateTime.getSecondOfMinute(),
-                dateTime.getMillisOfSecond() * 1_000_000 + (int) timeValue.getNSec()
-        );
-
-        statement.setObject(index, timestamp);
+        // PGJDBC uses strings internally anyway, so using Timestamp doesn't do any good
+        String tsString = PgDateTimeUtils.timestampValueToString(context, value, null, true);
+        statement.setObject(index, tsString, Types.OTHER);
     }
 
     private static void setTimestampInfinity(final PreparedStatement statement,
@@ -390,7 +352,8 @@ public class PostgreSQLRubyJdbcConnection extends arjdbc.jdbc.RubyJdbcConnection
         final int index, IRubyObject value,
         final IRubyObject attribute, final int type) throws SQLException {
         // to handle more fractional second precision than (default) 59.123 only
-        super.setTimestampParameter(context, connection, statement, index, value, attribute, type);
+        String timeStr = DateTimeUtils.timeString(context, value, DateTimeZone.UTC, true);
+        statement.setObject(index, timeStr, Types.OTHER);
     }
 
     @Override

src/java/arjdbc/util/DateTimeUtils.java
@@ -644,6 +644,25 @@ public abstract class DateTimeUtils {
         return sb.toString();
     }
 
+    /**
+     * Converts a ruby time to a java string, optionally with timezone and timezone adjustment
+     * @param context
+     * @param value the ruby value, typically a Time
+     * @param zone DateTimeZone to adjust to, optional
+     * @param withZone include timezone in the string?
+     * @return time as string
+     */
+    public static String timeString(final ThreadContext context,
+                                    final IRubyObject value, DateTimeZone zone, boolean withZone) {
+        StringBuilder sb = new StringBuilder(21);
+        RubyTime timeValue = toTime(context, value);
+        DateTime dt = timeValue.getDateTime();
+        if (zone != null) dt = dateTimeInZone(dt, zone);
+
+        appendTime(sb, dt.getChronology(), dt.getMillis(), (int) timeValue.getUSec(), withZone);
+        return sb.toString();
+    }
+
     private static void appendTime(StringBuilder sb, Chronology chrono,
                                    long millis, int usec, boolean withZone) {
         sb.append(NUMBERS[chrono.hourOfDay().get(millis)]);

commit cf5755db06b2b43baf9c82950de6d3e5c65eeecd

[postgres] fix time handling bug, fallout from 8cb5a2ac

src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
@@ -352,7 +352,7 @@ public class PostgreSQLRubyJdbcConnection extends arjdbc.jdbc.RubyJdbcConnection
         final int index, IRubyObject value,
         final IRubyObject attribute, final int type) throws SQLException {
         // to handle more fractional second precision than (default) 59.123 only
-        String timeStr = DateTimeUtils.timeString(context, value, DateTimeZone.UTC, true);
+        String timeStr = DateTimeUtils.timeString(context, value, getDefaultTimeZone(context), true);
         statement.setObject(index, timeStr, Types.OTHER);
     }
 