5798a00d743dc508f7e5ad4e44a0264e157c7760
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-01-21 13:03:47 +1100

    commet out some undefined methods/constants

--- lib/arjdbc/mssql/types.rb
+++ lib/arjdbc/mssql/types.rb
@@ -316,7 +316,8 @@ module ArJdbc
     class UUIDType < ActiveRecord::Type::String
       def type; :uuid end
 
-      alias_method :type_cast_for_database, :type_cast_from_database
+      # FIXME: type_cast_from_database is not defined anymore
+      # alias_method :type_cast_for_database, :type_cast_from_database
 
       ACCEPTABLE_UUID = %r{\A\{?([a-fA-F0-9]{4}-?){8}\}?\z}x
       def type_cast(value); value.to_s[ACCEPTABLE_UUID, 0] end
