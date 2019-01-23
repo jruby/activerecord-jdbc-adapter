995ffab561bee5ba86fac1f7699af0ab11bdec8d
Daniel Ritz <daniel.ritz@shellybits.ch>
2019-01-23 21:52:18 +0100

    [test] exclude multi-db tests for now
    These tests all try various multi-DB things, using an SQLite3 database. When
    running the Rails tests from AR-JDBC, the SQLite3 JARs are not on the search
    path so this fails.
    
    At a later point, the search path can be changed to include SQLite3 JARs,
    but for now this ensures we're actually testing PG/MySQL
    
    This bring the Rails test from 600+ errors down to a reasonable
      PostgreSQL: 34 failures, 1 errors
      MySQL: 31 failures, 0 errors

---
+++ test/rails/excludes/postgresql/ActiveRecord/ConnectionAdapters/ConnectionHandlerTest.rb
@@ -0,0 +1,15 @@
+#
+# These tests all try various multi-DB things, using an SQLite3 database. When
+# running the Rails tests from AR-JDBC, the SQLite3 JARs are not on the search
+# path so this fails.
+#
+# FIXME: add SQLite3 JARs to search path, but for now don't so we're actually
+#        sure we're testing with PostgreSQL
+#
+exclude :test_establish_connection_uses_spec_name, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_3_levels_config, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_3_level_config_defaults_to_default_env_primary_db, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_2_level_config_defaults_to_default_env_primary_db, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_two_level_configurations, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_top_level_key_in_two_level_config, 'tries to load SQLite3 driver'
+exclude :test_symbolized_configurations_assignment, 'tries to load SQLite3 driver'
