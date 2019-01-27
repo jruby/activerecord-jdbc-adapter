7632790efc7288e785d33b010ba2d86475c8f26e
Daniel Ritz <daniel.ritz@shellybits.ch>
2019-01-27 22:19:05 +0100

    [postgres] exclude some tests from CacheKeyTest
    PostgresSQL adapter with JDBC doesn't return string for updated_at, just
    like the Mysql2Adapter (native) that is already excluded in those tests.

---
+++ test/rails/excludes/postgresql/ActiveRecord/CacheKeyTest.rb
@@ -0,0 +1,3 @@
+exclude :test_cache_version_is_the_same_when_it_comes_from_the_DB_or_from_the_user, 'AR-JDBC version of the PostgreSQL adapter has does not return a string value for updated_at'
+exclude :test_cache_version_does_not_truncate_zeros_when_timestamp_ends_in_zeros, 'AR-JDBC version of the PostgreSQL adapter has does not return a string value for updated_at'
+exclude :test_cache_version_does_NOT_call_updated_at_when_value_is_from_the_database, 'AR-JDBC version of the PostgreSQL adapter has does not return a string value for updated_at'
