0dd3ecbadee40b15e2eb31ab010b6e56e90826be
Chris Robbie <chrisr@ibsglobalweb.com>
2019-01-31 13:58:57 +1100

    handle calls to execute stored procedures
    - pulled in exec_proc method from pre Rails 5 adapter
    - patch will now pass test exec_proc_test

--- lib/arjdbc/mssql/database_statements.rb
+++ lib/arjdbc/mssql/database_statements.rb
@@ -2,7 +2,28 @@ module ActiveRecord
   module ConnectionAdapters
     module MSSQL
       module DatabaseStatements
-        # to be implemented
+
+        def exec_proc(proc_name, *variables)
+          vars =
+            if variables.any? && variables.first.is_a?(Hash)
+              variables.first.map { |k, v| "@#{k} = #{quote(v)}" }
+            else
+              variables.map { |v| quote(v) }
+            end.join(', ')
+          sql = "EXEC #{proc_name} #{vars}".strip
+          log(sql, 'Execute Procedure') do
+            result = @connection.execute_query_raw(sql)
+            result.map! do |row|
+              row = row.is_a?(Hash) ? row.with_indifferent_access : row
+              yield(row) if block_given?
+              row
+            end
+            result
+          end
+        end
+        alias_method :execute_procedure, :exec_proc # AR-SQLServer-Adapter naming
+
+
       end
     end
   end
