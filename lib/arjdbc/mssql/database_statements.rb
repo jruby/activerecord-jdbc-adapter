2fb95c78fdc5ee8e6d1eb838c4b1f0c49e3bc00e
Chris Robbie <chrisr@ibsglobalweb.com>
2019-02-01 10:16:06 +1100

    handle identity on/off for inserts passed to execute
    included minimal to get identity_insert test passing.
    included minimal code from previous MSSQL::Column
    kept previous MSSQL::Column as old_column.rb

--- lib/arjdbc/mssql/database_statements.rb
+++ lib/arjdbc/mssql/database_statements.rb
@@ -23,6 +23,60 @@ module ActiveRecord
         end
         alias_method :execute_procedure, :exec_proc # AR-SQLServer-Adapter naming
 
+        def execute(sql, name = nil)
+          # with identity insert on block
+          if insert_sql?(sql)
+            if id_insert_table_name = identity_insert_table_name(sql)
+              with_identity_insert_enabled(id_insert_table_name) do
+                super
+              end
+            else
+              super
+            end
+          else
+            super
+          end
+        end
+
+
+        private
+
+        def insert_sql?(sql)
+          !(sql =~ /^\s*(INSERT|EXEC sp_executesql N'INSERT)/i).nil? 
+        end
+
+        def identity_insert_table_name(sql)
+          table_name = ArJdbc::MSSQL::Utils.get_table_name(sql)
+          id_column = identity_column_name(table_name)
+          if id_column && sql.strip =~ /INSERT INTO [^ ]+ ?\((.+?)\)/i
+            insert_columns = $1.split(/, */).map{|w| ArJdbc::MSSQL::Utils.unquote_column_name(w)}
+            return table_name if insert_columns.include?(id_column)
+          end
+        end
+
+        def identity_column_name(table_name)
+          for column in columns(table_name)
+            return column.name if column.identity
+          end
+          nil
+        end
+
+        # Turns IDENTITY_INSERT ON for table during execution of the block
+        # N.B. This sets the state of IDENTITY_INSERT to OFF after the
+        # block has been executed without regard to its previous state
+        def with_identity_insert_enabled(table_name)
+          set_identity_insert(table_name, true)
+          yield
+        ensure
+          set_identity_insert(table_name, false)
+        end
+
+        def set_identity_insert(table_name, enable = true)
+          execute "SET IDENTITY_INSERT #{table_name} #{enable ? 'ON' : 'OFF'}"
+        rescue Exception => e
+          raise ActiveRecord::ActiveRecordError, "IDENTITY_INSERT could not be turned" +
+                " #{enable ? 'ON' : 'OFF'} for table #{table_name} due : #{e.inspect}"
+        end
 
       end
     end
