2a756ed03b9705fff7160bc919a56faf29a977a6
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-02-04 11:18:14 +0900

    Make sqlserver default adapter when running tests

--- test/db/mssql_config.rb
+++ test/db/mssql_config.rb
@@ -1,13 +1,34 @@
-MSSQL_CONFIG = { :adapter  => 'mssql' }
-MSSQL_CONFIG[:database] = ENV['SQLDATABASE'] || 'weblog_development'
-MSSQL_CONFIG[:username] = ENV['SQLUSER'] || 'blog'
-MSSQL_CONFIG[:password] = ENV['SQLPASS'] || ''
+# currently sqlserver is the default driver
 
-MSSQL_CONFIG[:host] = ENV['SQLHOST'] || 'localhost'
+MSSQL_CONFIG = {
+  adapter:  'sqlserver',
+  database: ENV['SQLDATABASE'] || 'arjdbc_test',
+  username: ENV['SQLUSER'] || 'arjdbc',
+  password: ENV['SQLPASS'] || 'arjdbc',
+  host:     ENV['SQLHOST'] || 'localhost'
+}
+
 MSSQL_CONFIG[:port] = ENV['SQLPORT'] if ENV['SQLPORT']
 
 unless ( ps = ENV['PREPARED_STATEMENTS'] || ENV['PS'] ).nil?
   MSSQL_CONFIG[:prepared_statements] = ps
 end
 
-MSSQL_CONFIG[:adapter] = 'sqlserver' unless defined? JRUBY_VERSION
+
+if ENV['DRIVER'] =~ /jTDS/i
+  # change adapter for jTDS
+  MSSQL_CONFIG[:adapter] = 'mssql'
+else
+  # Using MS official  SQL JDBC driver
+  require 'jdbc/sqlserver'
+  begin
+    silence_warnings { Java::JavaClass.for_name(Jdbc::SQLServer.driver_name) }
+  rescue NameError
+    begin
+      Jdbc::SQLServer.load_driver
+    rescue LoadError => e
+      warn "Please setup the sqljdbc4.jar driver to run the MS-SQL tests !"
+      raise e
+    end
+  end
+end
