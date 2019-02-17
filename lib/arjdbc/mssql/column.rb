656b7cd671f2ac07b1bd1277bab631bb94b077d0
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-02-17 15:01:08 +1100

    Add mssql column class initialize and extract_default methods
    - This fixes column default values (fixes some tests)

--- lib/arjdbc/mssql/column.rb
+++ lib/arjdbc/mssql/column.rb
@@ -2,6 +2,16 @@ module ActiveRecord
   module ConnectionAdapters
     # MSSQL specific extensions to column definitions in a table.
     class MSSQLColumn < Column
+      def initialize(name, raw_default, sql_type_metadata = nil, null = true, table_name = nil, default_function = nil, collation = nil, comment: nil)
+        default = extract_default(raw_default)
+
+        super(name, default, sql_type_metadata, null, table_name, default_function, collation, comment: comment)
+      end
+
+      def extract_default(value)
+        return $1 if value =~ /^\(N?'(.*)'\)$/ || value =~ /^\(\(?(.*?)\)?\)$/
+        value
+      end
 
       def identity?
         !! sql_type.downcase.index('identity')
