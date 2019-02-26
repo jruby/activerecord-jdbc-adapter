40f9371aac8059929cf4ad2e96a5bbf52d0c7f4c
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-02-27 08:59:39 +1100

    move numeric types to a separate file

--- lib/arjdbc/mssql/types.rb
+++ lib/arjdbc/mssql/types.rb
@@ -1,67 +1,12 @@
+require 'arjdbc/mssql/types/numeric_types'
+
 # MSSQL type definitions
 module ActiveRecord
   module ConnectionAdapters
     module MSSQL
       module Type
 
-        class Integer < ActiveRecord::Type::Integer
-        end
-
-        class TinyInteger < ActiveRecord::Type::Integer
-          def max_value
-            256
-          end
-
-          def min_value
-            0
-          end
-        end
-
-        class SmallInteger < ActiveRecord::Type::Integer
-        end
-
-        class BigInteger < ActiveRecord::Type::Integer
-          def type
-            :bigint
-          end
-        end
-
         class Boolean < ActiveRecord::Type::Boolean
-        end
-
-        class Decimal < ActiveRecord::Type::Decimal
-        end
-
-        class Money < Decimal
-          def initialize(options = {})
-            super
-            @precision = 19
-            @scale = 4
-          end
-          def type
-            :money
-          end
-        end
-
-        class SmallMoney < Decimal
-          def initialize(options = {})
-            super
-            @precision = 10
-            @scale = 4
-          end
-
-          def type
-            :smallmoney
-          end
-        end
-
-        class Float < ActiveRecord::Type::Float
-        end
-
-        class Real < ActiveRecord::Type::Float
-          def type
-            :real
-          end
         end
 
       end
