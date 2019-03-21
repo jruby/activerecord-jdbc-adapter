e62b3fdca5b2f044a5a478c93735d9ca64b4f586
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-03-22 07:59:55 +1100

    Add some support_* flags relevant to the mssql adapter

--- lib/arjdbc/mssql/adapter.rb
+++ lib/arjdbc/mssql/adapter.rb
@@ -74,13 +74,24 @@ module ActiveRecord
         ::ActiveRecord::ConnectionAdapters::MSSQLColumn
       end
 
+      # Can this adapter determine the primary key for tables not attached
+      # to an Active Record class, such as join tables?
+      def supports_primary_key?
+        true
+      end
+
+      # Does this adapter support creating foreign key constraints?
+      def supports_foreign_keys?
+        false
+      end
+
       # Does this adapter support migrations?
       def supports_migrations?
         true
       end
 
       # Does this adapter support setting the isolation level for a transaction?
-      def supports_transaction_isolation?(level = nil)
+      def supports_transaction_isolation?
         false
       end
 
