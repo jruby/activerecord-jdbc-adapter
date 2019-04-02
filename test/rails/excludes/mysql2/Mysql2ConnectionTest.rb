afaa85305dccdf4122d4d6f0f22179dced77f2bb
Daniel Ritz <daniel.ritz@shellybits.ch>
2019-04-02 21:08:43 +0200

    [mysql] Update some rails test excludes

--- test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
+++ test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
@@ -1,6 +1,7 @@
-exclude :test_passing_arbitary_flags_to_adapter, 'no support for arbitrary flags such as Mysql2::Client::COMPRESS'
+exclude :test_passing_arbitrary_flags_to_adapter, 'no support for arbitrary flags such as Mysql2::Client::COMPRESS'
 exclude :test_passing_flags_by_array_to_adapter, 'no support for passing flags: ... not supported by JDBC adapter'
 exclude :test_quote_after_disconnect, 'AR-JDBC does not rely on driver to do quoting - thus wont raise an error'
+exclude :test_execute_after_disconnect, "test relying on mysql2 internals"
 exclude :test_mysql_connection_collation_is_configured, "Issue #884"
 
 if ActiveRecord::Base.connection.jdbc_connection(true).java_class.name.start_with?('org.mariadb.jdbc')
