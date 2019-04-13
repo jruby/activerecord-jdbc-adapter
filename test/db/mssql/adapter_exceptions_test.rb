640666583c50b2be02bbddc98d30a5c7dcd3bbe6
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-04-13 11:59:50 +1000

    Rework on exceptions test file to add more exeption tests

--- test/db/mssql/adapter_exceptions_test.rb
+++ test/db/mssql/adapter_exceptions_test.rb
@@ -2,39 +2,52 @@ require 'test_helper'
 require 'db/mssql'
 
 class MSSQLExceptionsTest < Test::Unit::TestCase
-  class CreateExceptionTests < ActiveRecord::Migration
+  class CreateSystemExceptions < ActiveRecord::Migration
     def self.up
-      create_table :exception_tests do |t|
-        t.string :email
+      create_table :system_exceptions do |t|
+        t.string :name, limit: 50
+        t.text :notes
+
+        t.timestamps
+      end
+      create_table :exception_sources do |t|
+        t.references :system_exception, foreign_key: true
+        t.string :source_name, limit: 20
+        t.string :source_ip
+        t.binary :logfile
 
         t.timestamps
       end
 
-      add_index :exception_tests, :email, unique: true
+      add_index :system_exceptions, :name, unique: true
     end
 
     def self.down
-      drop_table :exception_tests
+      drop_table :exception_sources
+      drop_table :system_exceptions
     end
   end
 
-  class ExceptionTest < ActiveRecord::Base
+  class SystemException < ActiveRecord::Base
+  end
+
+  class ExceptionSource < ActiveRecord::Base
   end
 
   def setup
-    CreateExceptionTests.up
+    CreateSystemExceptions.up
   end
 
   def teardown
-    CreateExceptionTests.down
+    CreateSystemExceptions.down
     ActiveRecord::Base.clear_active_connections!
   end
 
   def test_uniqueness_violations_are_translated_to_specific_exception
-    ExceptionTest.create!(email: 'cool@awesome.me')
+    SystemException.create!(name: 'uniqueness_violation')
 
     error = assert_raises(ActiveRecord::RecordNotUnique) do
-      ExceptionTest.create!(email: 'cool@awesome.me')
+      SystemException.create!(name: 'uniqueness_violation')
     end
 
     assert_not_nil error.cause
