f7013517a1703c9f58ee27eca9416b597c769336
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-04-16 10:38:23 +1000

    Add some relevant notes

--- lib/arjdbc/mssql.rb
+++ lib/arjdbc/mssql.rb
@@ -4,4 +4,4 @@ require 'arjdbc/mssql/connection_methods'
 module ArJdbc
   MsSQL = MSSQL # compatibility with 1.2
 end
-ArJdbc.warn_unsupported_adapter 'mssql', [4, 2] # warns on AR >= 4.2
+ArJdbc.warn_unsupported_adapter 'mssql', [5, 0] # warns on AR >= 4.2
