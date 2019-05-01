2aa3f16f18b2778f50269077b186e1e55bd1c623
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-05-01 22:36:12 +1000

    Add other mssql specific schema definitions

--- lib/arjdbc/mssql/schema_definitions.rb
+++ lib/arjdbc/mssql/schema_definitions.rb
@@ -12,6 +12,54 @@ module ActiveRecord
         def datetime_basic(*args, **options)
           args.each { |name| column(name, :datetime_basic, options) }
         end
+
+        def real(*args, **options)
+          args.each { |name| column(name, :real, options) }
+        end
+
+        def money(*args, **options)
+          args.each { |name| column(name, :money, options) }
+        end
+
+        def smallmoney(*args, **options)
+          args.each { |name| column(name, :smallmoney, options) }
+        end
+
+        def char(*args, **options)
+          args.each { |name| column(name, :char, options) }
+        end
+
+        def varchar(*args, **options)
+          args.each { |name| column(name, :varchar, options) }
+        end
+
+        def varchar_max(*args, **options)
+          args.each { |name| column(name, :varchar_max, options) }
+        end
+
+        def text_basic(*args, **options)
+          args.each { |name| column(name, :text_basic, options) }
+        end
+
+        def nchar(*args, **options)
+          args.each { |name| column(name, :nchar, options) }
+        end
+
+        def ntext(*args, **options)
+          args.each { |name| column(name, :ntext, options) }
+        end
+
+        def binary_basic(*args, **options)
+          args.each { |name| column(name, :binary_basic, options) }
+        end
+
+        def varbinary(*args, **options)
+          args.each { |name| column(name, :varbinary, options) }
+        end
+
+        def uuid(*args, **options)
+          args.each { |name| column(name, :uniqueidentifier, options) }
+        end
       end
 
       class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
