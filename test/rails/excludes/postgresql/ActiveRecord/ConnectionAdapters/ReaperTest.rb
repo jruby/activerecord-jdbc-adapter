94c43313a784664b3f53e8dff3fdf7b9d5897598
Daniel Ritz <daniel.ritz@shellybits.ch>
2019-08-28 22:24:06 +0200

    [test] exclude Rails tests using fork

--- test/rails/excludes/postgresql/ActiveRecord/ConnectionAdapters/ReaperTest.rb
+++ test/rails/excludes/postgresql/ActiveRecord/ConnectionAdapters/ReaperTest.rb
@@ -1,2 +1,3 @@
 exclude :test_some_time, 'intermittent failures, leaks thread, fires at high frequency'
 exclude :test_connection_pool_starts_reaper, 'intermittent failures, leaks thread, fires at high frequency'
+exclude :test_connection_pool_starts_reaper_in_fork, 'fork not supported in JRuby'
