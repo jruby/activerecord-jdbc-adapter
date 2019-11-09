51b6f322ec2c38c9dc4267b6f883d86b160c010a
Daniel Ritz <daniel.ritz@shellybits.ch>
2019-11-09 20:09:06 +0100

    [test] exclude test for PG/mysql that try to load the sqlite3 driver

---
+++ test/rails/excludes/postgresql/ActiveRecord/ConnectionAdapters/ConnectionHandlersMultiPoolConfigTest.rb
@@ -0,0 +1,3 @@
+exclude :test_establish_connection_with_pool_configs, 'tries to load SQLite3 driver'
+exclude :test_remove_connection, 'tries to load SQLite3 driver'
+exclude :test_connected?, 'tries to load SQLite3 driver'
