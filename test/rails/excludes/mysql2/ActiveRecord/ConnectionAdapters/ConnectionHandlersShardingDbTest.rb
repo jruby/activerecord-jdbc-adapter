0988d7e26bf1c86dd8a5a71842e558f422776868
Daniel Ritz <daniel.ritz@shellybits.ch>
2020-03-03 21:18:24 +0100

    [test] Add some excludes for tests trying to load sqlite3

---
+++ test/rails/excludes/mysql2/ActiveRecord/ConnectionAdapters/ConnectionHandlersShardingDbTest.rb
@@ -0,0 +1,13 @@
+#
+# These tests all try various multi-DB things, using an SQLite3 database. When
+# running the Rails tests from AR-JDBC, the SQLite3 JARs are not on the search
+# path so this fails.
+#
+exclude :test_establish_connection_using_3_levels_config, 'tries to load SQLite3 driver'
+exclude :test_establish_connection_using_3_levels_config_with_shards_and_replica, 'tries to load SQLite3 driver'
+exclude :test_switching_connections_via_handler, 'tries to load SQLite3 driver'
+exclude :test_retrieves_proper_connection_with_nested_connected_to, 'tries to load SQLite3 driver'
+exclude :test_same_shards_across_clusters, 'tries to load SQLite3 driver'
+exclude :test_sharding_separation, 'tries to load SQLite3 driver'
+exclude :test_swapping_shards_in_a_multi_threaded_environment, 'tries to load SQLite3 driver'
+exclude :test_swapping_shards_and_roles_in_a_multi_threaded_environment, 'tries to load SQLite3 driver'
