82ef2a098543080f15b496f7724f0eb54f424ed5
Daniel Ritz <daniel.ritz@shellybits.ch>
2020-11-07 23:14:31 +0100

    test(rails): provide empty byebug.rb
    Rails started to include byebug in tests, but that one doesn't support
    JRuby. Since it's only required and nothing more, providing an empty
    file on the include path is enough to get things going again.

---
+++ test/rails/byebug.rb
@@ -0,0 +1,4 @@
+# empty placeholder for byebug:
+# Rails started to include byebug in tests, but that one doesn't support
+# JRuby. Since it's only required and nothing more, providing an empty
+# file on the include path is enough to get things going again.
