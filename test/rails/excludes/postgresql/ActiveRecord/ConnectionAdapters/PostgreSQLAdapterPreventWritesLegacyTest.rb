9614afd376de3db75f5a6ddd3f3790efd1e3e728
Daniel Ritz <daniel.ritz@shellybits.ch>
2020-11-08 17:25:05 +0100

    test(rails): move some existing excludes to their new place

---
+++ test/rails/excludes/postgresql/ActiveRecord/ConnectionAdapters/PostgreSQLAdapterPreventWritesLegacyTest.rb
@@ -0,0 +1,2 @@
+exclude :test_doesnt_error_when_a_read_query_with_cursors_is_called_while_preventing_writes, 'different return value for execute() in ARJCBC'
+exclude :test_doesnt_error_when_a_set_query_is_called_while_preventing_writes, 'different return value for execute() in ARJCBC'
