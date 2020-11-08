2fda2e028e8847f22ada551740d6b98a08a484f3
Daniel Ritz <daniel.ritz@shellybits.ch>
2020-11-08 22:06:12 +0100

    test(rails): exclude test that has env-specific error

--- test/rails/excludes/mysql2/Mysql2AdapterTest.rb
+++ test/rails/excludes/mysql2/Mysql2AdapterTest.rb
@@ -1,3 +1,4 @@
 exclude :test_statement_timeout_error_codes, 'test stubs #query and expects #execute to call it, but arjdbc has its own implementation'
 exclude :test_read_timeout_exception, 'uses "read_timeout" option, unknown to JDBC driver'
 exclude :test_reconnection_error, 'different internals'
+exclude :test_connection_error, 'error is environment specific'
