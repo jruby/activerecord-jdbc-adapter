2d3aa7766f3a65bff14591c8e222a8a240bd5af6
Jesse Chavez <jesse.chavez.r@gmail.com>
2024-07-31 23:25:54 +1000

    Delete test about loading postgres jdbc driver for java 7

--- test/db/postgresql/jdbc_postgres_test.rb
+++ test/db/postgresql/jdbc_postgres_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 module Jdbc
   class PostgresTest < Test::Unit::TestCase
-
-    SKIP_TESTS = false
     SYSTEM_ENV = ENV_JAVA.dup
 
     setup do
@@ -16,37 +14,16 @@ module Jdbc
 
     test('some') { assert Jdbc::Postgres }
 
-    test 'returns jdbc version 4.1 on java 7' do
-      ENV_JAVA[ 'java.specification.version' ] = '1.7'
-      assert_equal 7, Jdbc::Postgres.send(:jre_version)
-    end unless SKIP_TESTS
+    test 'returns jdbc version default (4.2) on java 8' do
+      ENV_JAVA['java.specification.version'] = '1.8'
 
-    test 'returns jdbc version default (4.2) on java 8/9' do
-      ENV_JAVA[ 'java.specification.version' ] = '1.8'
       assert_nil Jdbc::Postgres.send(:jre_version)
-
-      ENV_JAVA[ 'java.specification.version' ] = '9'
-      assert_nil Jdbc::Postgres.send(:jre_version)
-    end unless SKIP_TESTS
-
-    context 'load-driver' do
-
-      ROOT_DIR = File.expand_path('../../..', File.dirname(__FILE__))
-
-      @@driver_dir = File.join(ROOT_DIR, 'jdbc-postgres/lib')
-
-      test 'on java 7' do
-        ENV_JAVA[ 'java.specification.version' ] = '1.7'
-        Jdbc::Postgres.expects(:load).with do |driver_jar|
-          assert_match(/.jre7.jar$/, driver_jar)
-          full_path = File.join(@@driver_dir, driver_jar)
-          assert File.exist?(full_path), "#{driver_jar.inspect} not found in: #{@@driver_dir.inspect}"
-          true
-        end
-        Jdbc::Postgres.load_driver
-      end
-
     end
 
-  end if defined? JRUBY_VERSION
+    test 'returns jdbc version 4.2 on java 11' do
+      ENV_JAVA['java.specification.version'] = '11'
+
+      assert_nil Jdbc::Postgres.send(:jre_version)
+    end
+  end
 end
