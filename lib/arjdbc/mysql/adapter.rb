c7eb4e438ea538ed3599680952525fad4edcafa7
Jesse Chavez <jesse.chavez.r@gmail.com>
2025-02-04 07:04:35 +1100

    Mysql, fix undefined database_version to run rails tests

--- lib/arjdbc/mysql/adapter.rb
+++ lib/arjdbc/mysql/adapter.rb
@@ -250,7 +250,7 @@ module ActiveRecord
 
       # e.g. "5.7.20-0ubuntu0.16.04.1"
       def full_version
-        schema_cache.database_version.full_version_string
+        database_version.full_version_string
       end
 
       def get_full_version
