0b8612ae0ec3ea2f7a0da7c69ff12e672a26984f
Jesse Chavez <jesse.chavez.r@gmail.com>
2025-02-04 08:20:27 +1100

    Mysql, fix some excludes to be able runn AR tests

--- test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
+++ test/rails/excludes/mysql2/Mysql2ConnectionTest.rb
@@ -5,6 +5,6 @@ exclude :test_execute_after_disconnect, "test relying on mysql2 internals"
 exclude :test_mysql_connection_collation_is_configured, "Issue #884"
 exclude :test_wait_timeout_as_url, 'uses URL format unknown to JDBC driver (i.e. "mysql2:///")'
 
-if ActiveRecord::Base.connection.jdbc_connection(true).java_class.name.start_with?('org.mariadb.jdbc')
-  exclude :test_mysql_strict_mode_specified_default, 'MariaDB driver reports more options'
-end
+# if ActiveRecord::Base.connection.jdbc_connection(true).java_class.name.start_with?('org.mariadb.jdbc')
+#   exclude :test_mysql_strict_mode_specified_default, 'MariaDB driver reports more options'
+# end
