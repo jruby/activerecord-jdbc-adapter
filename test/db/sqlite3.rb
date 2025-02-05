4937a6f095b4dcdbc20a112758aa07670d247d9f
Jesse Chavez <jesse.chavez.r@gmail.com>
2025-02-05 19:13:31 +1100

    Sqlite, ensure we delete the database when start running tests

--- test/db/sqlite3.rb
+++ test/db/sqlite3.rb
@@ -10,4 +10,8 @@ unless ( ps = ENV['PREPARED_STATEMENTS'] || ENV['PS'] ).nil?
 end
 ActiveRecord::Base.establish_connection(SQLITE3_CONFIG)
 
-at_exit { Dir['*test.sqlite3-*'].each { |f| File.delete(f) } }
+at_exit do
+  Dir["*test.sqlite3*"].each do |sqlite_file|
+    File.delete(sqlite_file)
+  end
+end
